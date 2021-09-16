042a3d8b907dfcd59a23b3c047a60747a29964eb
TomAndersen0714 <1040994898@qq.com>
2021-09-17 01:31:48 +0800

    master

--- program/SQL/clickhouse/tmp/test.sql
+++ program/SQL/clickhouse/tmp/test.sql
@@ -1,147 +1,176 @@
+-- 方案一: 路径枚举表
+xqc_dim.company_department_all:
+  department_id
+  department_name
+  is_shop
+  parent_department_path
+  level
+  company_id
+  company_name
+  create_time
+  update_time
 
-SELECT 
-    question, question_id, qoid, pv
-FROM (
-    SELECT qid, question
-    FROM dim.question_b
-) AS dim
-RIGHT JOIN(
-    SELECT 
-        question_id, qoid, pv
-    FROM 
-        app_mp.stat_question_for_shop
-    WHERE 
-        day = CAST(replace('{{ day }}','-','') AS INT)
-        AND snick_oid = '{{ shop_id }}'
-    ORDER BY pv DESC
-    LIMIT 20
-) AS ods
-ON dim.qid = ods.qoid
+-- 一级部门
+BG:
+SELECT department_name AS BG
+FROM xqc_dim.company_department_all
+WHERE company_id = '61372098699003a721a63a51' AND level = 1
+-- 二级部门
+BU:
+SELECT department_id, department_name
+FROM xqc_dim.company_department_all
+WHERE company_id = '61372098699003a721a63a51' AND level = 2
+-- 查询一级部门和snick的映射
+SELECT department_id, department_name, snick
+FROM xqc_dim.company_department_all
+LEFT JOIN xqc_dim.snick
+USING company_id
+WHERE company_id = '61372098699003a721a63a51'
+AND level = 1
+
+-- 方案二: 关系枚举表
+xqc_dim.company_department_all:
+  department_id
+  department_name
+  is_shop
+  parent_department
+  level
+  company_id
+  company_name
+  create_time
+  update_time
+-- 一级部门
+SELECT distinct department_id, department_name
+FROM xqc_dim.company_department_all
+WHERE level=1
+
+-- 二级部门
+SELECT distinct department_id, department_name
+FROM xqc_dim.company_department_all
+WHERE level=2
 
 
-DROP TABLE tmp.test_tbl_1_local ON CLUSTER cluster_3s_2r
-CREATE TABLE tmp.test_tbl_1_local ON CLUSTER cluster_3s_2r
-AS tmp.test_tbl_local
-ENGINE = ReplicatedMergeTree('/clickhouse/tmp/tables/{layer}_{shard}/test_tbl_1_local', '{replica}')
-PARTITION BY version
-ORDER BY (_id, version)
-SETTINGS index_granularity = 8192, storage_policy = 'default'
 
-DROP TABLE tmp.test_tbl_1_all ON CLUSTER cluster_3s_2r
-CREATE TABLE tmp.test_tbl_1_all ON CLUSTER cluster_3s_2r
-AS tmp.test_tbl_1_local
-ENGINE = Distributed('cluster_3s_2r', 'tmp', 'test_tbl_1_local', rand())
-
-
-TRUNCATE TABLE tmp.test_tbl_1_local ON CLUSTER cluster_3s_2r
-OPTIMIZE TABLE tmp.test_tbl_1_local ON CLUSTER cluster_3s_2r
-
-CREATE TABLE test.biz_log 
-(
-    `ts` DateTime, 
-    `tag` String, 
-    `message` String
-) 
-ENGINE = Kafka 
-SETTINGS kafka_broker_list = '10.0.2.0:9092,10.0.2.1:9092,10.0.2.2:9092',
-kafka_topic_list = 'tag',
-kafka_group_name = 'bigdata',
-kafka_format = 'JSONEachRow',
-kafka_skip_broken_messages = 1,
-kafka_num_consumers = 2
-
-
--- CREATE DATABASE dipper ON cluster cluster_3s_2r
-CREATE TABLE dipper.ask_order_conversion_stat_day_local ON cluster cluster_3s_2r( 
-    `day` Int32,
-    `shop_id` String,  
-    `platform` String, 
-    `ao_category` String,  
-    `ao_total_consult_order_cuv` Int64, 
-    `ao_ordered_cuv` Int64, 
-    `ao_paid_cuv` Int64, 
-    `ao_ordered_volume` Int64, 
-    `ao_sold_money_volume` Float32, 
-    `ao_avg_transaction_value` Float32
+-- 告警表
+CREATE TABLE tmp.event_alert_local ON CLUSTER cluster_3s_2r (
+    `id` String,
+    `level` Int64,
+    `warning_type` String,
+    `dialog_id` String,
+    `message_id` String,
+    `time` String,
+    `day` Int64,
+    `is_finished` String,
+    `finish_time` String,
+    `update_time` DateTime
 )
-ENGINE = ReplicatedMergeTree('/clickhouse/dipper/tables/{layer}_{shard}/ask_order_conversion_stat_day_local', '{replica}')  
-PARTITION BY day 
-ORDER BY (shop_id,platform) SETTINGS index_granularity = 8192
-
-CREATE TABLE dipper.ask_order_conversion_stat_day_all on cluster cluster_3s_2r
-as dipper.ask_order_conversion_stat_day_local
-ENGINE = Distributed('cluster_3s_2r', 'dipper', 'ask_order_conversion_stat_day_local', rand())
-
-
-SELECT distinct
-    _id,
-    if((neighbor(_id,-1,'') == _id),neighbor(version,-1),version)
-FROM (
-    SELECT *
-    FROM tmp.test_tbl_all
-    ORDER BY _id,version DESC
-) AS t1
-
-
-
-CREATE TABLE alerts_amt_max (
-    tenant_id UInt32,
-    alert_id String,
-    timestamp DateTime Codec(Delta, LZ4),
-    alert_data SimpleAggregateFunction(max, String),
-    acked SimpleAggregateFunction(max, UInt8),
-    ack_time SimpleAggregateFunction(max, DateTime),
-    ack_user SimpleAggregateFunction(max, LowCardinality(String))
-) Engine = AggregatingMergeTree()
-ORDER BY (tenant_id, timestamp, alert_id);
-
-CREATE TABLE xqc_ods.xdqc_tb_task_record_back_local
-AS xqc_ods.xdqc_tb_task_record_local
-ENGINE = MergeTree()
-ORDER BY mp_shop_id
-SETTINGS index_granularity = 8192, storage_policy = 'rr'
-
-
--- DROP TABLE xqc_ods.xdqc_tb_task_record_local ON CLUSTER cluster_3s_2r
-CREATE TABLE xqc_ods.xdqc_tb_task_record_local ON CLUSTER cluster_3s_2r
-(
-    `_id` String,
-    `platform` String,
-    `channel` String,
-    `seller_nick` String,
-    `group` String,
-    `date` Int64,
-    `account_name` String,
-    `account_id` String,
-    `task_mode` Int64,
-    `dialog_count` Int64,
-    `abnormal_dialog_count` Int64,
-    `mark_dialog_count` Int64,
-    `mp_shop_id` String
-) ENGINE = ReplicatedMergeTree(
-    '/clickhouse/xqc_ods/tables/{layer}_{shard}/xdqc_tb_task_record_local',
+ENGINE = ReplicatedMergeTree(
+    '/clickhouse/tmp/tables/{layer}_{shard}/event_alert_local',
     '{replica}'
-) 
-PARTITION BY (`date`, platform)
-ORDER BY (mp_shop_id, account_id) 
-SETTINGS index_granularity = 8192, storage_policy = 'rr'
+)
+ORDER BY (`dialog_id`,`id`)
+SETTINGS index_granularity=8192, storage_policy='rr'
 
--- DROP TABLE xqc_ods.xdqc_tb_task_record_all ON CLUSTER cluster_3s_2r
-CREATE TABLE xqc_ods.xdqc_tb_task_record_all ON CLUSTER cluster_3s_2r
-AS xqc_ods.xdqc_tb_task_record_local
-ENGINE = Distributed('cluster_3s_2r','xqc_ods','xdqc_tb_task_record_local',rand())
+CREATE TABLE tmp.event_alert_all ON CLUSTER cluster_3s_2r
+AS tmp.event_alert_local
+ENGINE = Distributed('cluster_3s_2r', 'tmp', 'event_alert_local', rand())
+
+-- 填充数据
+INSERT INTO tmp.event_alert_all
+SELECT
+    id, 
+    type AS level,
+    reason AS warning_type,
+    dialog_id,
+    '' AS message_id,
+    create_time AS time,
+    day,
+    done as is_finished,
+    now() as finished_time,
+    now() as update_time
+FROM xqc_ods.event_alert_all
 
 
-INSERT INTO xqc_ods.xdqc_tb_task_record_all
-SELECT * FROM xqc_ods.xdqc_tb_task_record_back_local WHERE `date` <=20210815
+-- 会话表
+CREATE TABLE tmp.dialog_local ON CLUSTER cluster_3s_2r(
+    `id` String,
+    `platform` String,
+    `shop_id` String,
+    `seller_nick`  String,
+    `snick` String,
+    `cnick` String,
+    `employee_name` String,
+    `superior_name` String,
+    `time` String,
+    `hour` Int64,
+    `day` Int64
+)
+ENGINE = ReplicatedMergeTree(
+    '/clickhouse/tmp/tables/{layer}_{shard}/dialog_local',
+    '{replica}'
+)
+PARTITION BY (`day`, `platform`)
+ORDER BY `snick`
+SETTINGS index_granularity=8192, storage_policy='rr'
 
-INSERT INTO xqc_ods.xdqc_tb_task_record_all
-SELECT * FROM xqc_ods.xdqc_tb_task_record_back_local WHERE `date` >20210815
+CREATE TABLE tmp.dialog_all ON CLUSTER cluster_3s_2r 
+AS tmp.dialog_local
+ENGINE = Distributed('cluster_3s_2r', 'tmp', 'dialog_local', rand())
+
+-- 填充数据
+INSERT INTO tmp.dialog_all
+SELECT
+  dialog_id AS id,
+  platform,
+  shop_id,
+  seller_nick,
+  snick,
+  cnick,
+  '' AS employee_name,
+  '' AS superior_name,
+  update_time AS time,
+  toHour(parseDateTimeBestEffort(update_time)) AS hour,
+  day
+FROM xqc_ods.event_alert_all
+WHERE day=20210915
+
+INSERT INTO xqc_ods.dialog_all
+SELECT
+  dialog_id AS id,
+  platform,
+  shop_id,
+  seller_nick,
+  snick,
+  cnick,
+  '' AS employee_name,
+  '' AS superior_name,
+  update_time AS time,
+  toHour(parseDateTimeBestEffort(update_time)) AS hour,
+  day
+FROM xqc_ods.event_alert_all
+WHERE day BETWEEN 20210814 AND 20210914
+
+-- 导出数据
+
+docker exec -i 96 clickhouse-client \
+--port=19000 --query="select * from xqc_dim.group_all FORMAT Parquet" > /tmp/group.parq
+
+docker exec -i 96 clickhouse-client \
+--port=19000 --query="select * from xqc_dim.snick_all FORMAT Parquet" > /tmp/snick.parq
+
+docker exec -i 9e clickhouse-client \
+--port=19000 --query="INSERT INTO xqc_dim.group_all FORMAT Parquet" < /tmp/group.parq
+
+docker exec -i 9e clickhouse-client \
+--port=19000 --query="INSERT INTO xqc_dim.snick_all FORMAT Parquet" < /tmp/snick.parq
+
+docker exec -i 9e clickhouse-client \
+--port=19000 --query="INSERT INTO xqc_dim.group_all FORMAT Arrow" < /tmp/group.arrow
+docker exec -i 9e clickhouse-client \
+--port=19000 --query="INSERT INTO xqc_dim.snick_all FORMAT Arrow" < /tmp/snick.arrow
 
 
 
-WITH 1 AS num_1, 2 AS num_2
-SELECT num_1,num_2
-
-WITH (SELECT 1) AS num_1, (SELECT 2) AS num_2
-SELECT num_1,num_2
+SELECT distinct snick
+FROM xqc_dim.snick_all
+WHERE shop_id IN ({{shop_ids=shop_list}})
