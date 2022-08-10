b65754dbc1f7cdc1dfcdf6a997b0e7f22f453c0b
TomAndersen <1040994898@qq.com>
2022-08-10 09:41:50 +0800

    update

--- my-workspace/program/SQL/XiaoDuoAI/明察质检/tmp1.sql
+++ my-workspace/program/SQL/XiaoDuoAI/明察质检/tmp1.sql
@@ -1,9 +1,24 @@
-_id String
-create_time String
-update_time String
-platform String
-company_id String
-creator String
-name String
-type Int32
-account_id String
+            SELECT account_employee_info.company_id AS company_id,
+                account_employee_info.account_id AS account_id,
+                account_employee_info.username AS username,
+                account_employee_info.employee_id AS employee_id
+            FROM (
+                SELECT account_info.company_id AS company_id,
+                    account_info.account_id AS account_id,
+                    employee_info.username AS username,
+                    account_info.employee_id AS employee_id
+                FROM (
+                    SELECT company_id,
+                        _id AS account_id,
+                        employee_id
+                    FROM ods.xinghuan_account_all
+                    WHERE day = {snapshot_ds_nodash}
+                ) AS account_info
+                LEFT JOIN (
+                    SELECT _id AS employee_id,
+                        username
+                    FROM ods.xinghuan_employee_all
+                    WHERE day = {snapshot_ds_nodash}
+                ) AS employee_info 
+                USING(employee_id)
+            ) AS account_employee_info
