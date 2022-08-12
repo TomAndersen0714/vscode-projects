8e2a1fc2b3d93e017a6e23453142dc4fc52ccd52
TomAndersen <1040994898@qq.com>
2022-08-13 00:48:50 +0800

    update

--- my-workspace/program/SQL/XiaoDuoAI/明察质检/数据监控/质检诊断报告/质检诊断报告-下拉框-获取店铺.sql
+++ my-workspace/program/SQL/XiaoDuoAI/明察质检/数据监控/质检诊断报告/质检诊断报告-下拉框-获取店铺.sql
@@ -5,6 +5,6 @@ FROM xqc_dim.xqc_shop_all
 WHERE day=toYYYYMMDD(yesterday())
 -- 筛选指定平台
 AND platform = 'tb'
--- 筛选指定企业的质检标准
+-- 筛选指定企业的店铺
 AND company_id = '{{ company_id=5f747ba42c90fd0001254404 }}'
 ORDER BY seller_nick COLLATE 'zh'
--- my-workspace/program/SQL/XiaoDuoAI/明察质检/数据监控/质检诊断报告/质检诊断报告-每日合格会话统计.sql
+++ my-workspace/program/SQL/XiaoDuoAI/明察质检/数据监控/质检诊断报告/质检诊断报告-每日合格会话统计.sql
@@ -32,5 +32,25 @@ AND seller_nick IN (
         seller_nick IN splitByChar(',', '{{ seller_nicks }}')
     )
 )
+-- 筛选指定质检标准对应的子账号
+AND (
+    '{{ qc_norm_ids }}'=''
+    OR
+    snick GLOBAL IN (
+        -- 筛选指定子账号分组中的子账号
+        SELECT snick
+        FROM ods.xinghuan_employee_snick_all
+        WHERE day = toYYYYMMDD(yesterday())
+        AND company_id = '{{ company_id=5f747ba42c90fd0001254404 }}'
+        AND department_id IN (
+            -- 筛选指定质检标准对应的子账号分组
+            SELECT department_id
+            FROM ods.xinghuan_qc_norm_relate_all
+            WHERE day = toYYYYMMDD(yesterday())
+            AND qc_norm_id IN splitByChar(',', '{{ qc_norm_ids }}')
+            AND company_id = '{{ company_id=5f747ba42c90fd0001254404 }}'
+        )
+    )
+)
 GROUP BY toYYYYMMDD(begin_time)
 ORDER BY day ASC
