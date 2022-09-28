0f53b909b443713f869ea89494741d06e68a5cf3
TomAndersen <1040994898@qq.com>
2022-09-28 22:56:33 +0800

    update

--- my-workspace/program/SQL/XiaoDuoAI/明察质检/tmp1.sql
+++ my-workspace/program/SQL/XiaoDuoAI/明察质检/tmp1.sql
@@ -1,19 +1,30 @@
--- DROP TABLE tmp.ft_dwd_order_detail_local ON CLUSTER cluster_3s_2r NO DELAY
-CREATE TABLE tmp.ft_dwd_order_detail_local ON CLUSTER cluster_3s_2r
-AS ft_dwd.order_detail_all
-ENGINE = ReplicatedMergeTree(
-    '/clickhouse/{database}/tables/{layer}_{shard}/{table}',
-    '{replica}'
-)
-ORDER BY (order_id, status, goods_id)
-SETTINGS storage_policy = 'rr', index_granularity = 8192
-
--- DROP TABLE tmp.ft_dwd_order_detail_all ON CLUSTER cluster_3s_2r NO DELAY
-CREATE TABLE tmp.ft_dwd_order_detail_all ON CLUSTER cluster_3s_2r
-AS tmp.ft_dwd_order_detail_local
-ENGINE = Distributed('cluster_3s_2r', 'tmp', 'ft_dwd_order_detail_local', rand())
-
-
-docker exec -i 9043cb24167c clickhouse-client --port=29000 --query \
-"INSERT INTO tmp.ft_dwd_order_detail_all FORMAT Parquet" \
-< /opt/bigdata/ft_dwd.order_detail_all_20220101_20220926.parquet
+SELECT
+    msg_time_delta_min_within_session,
+    COUNT(1) AS cnt
+FROM (
+    SELECT
+        day, platform, shop_id, snick, cnick,
+        groupArray(msg_time) AS msg_times,
+        arrayPushFront(arrayPopBack(msg_times), msg_times[1]) AS pre_msg_times,
+        arrayMap((x,y)->( toInt32((toDateTime(x)-toDateTime(y))/60) ), msg_times, pre_msg_times) AS msg_time_delta_mins_within_session
+    FROM (
+        SELECT 
+            day, platform, shop_id, snick, cnick, msg_time
+        FROM ods.xdrs_logs_all
+        WHERE day = {{ds_nodash}}
+        AND shop_id GLOBAL IN (
+            SELECT DISTINCT
+                shop_id
+            FROM xqc_dim.xqc_shop_all
+            WHERE day = toYYYYMMDD(yesterday())
+            AND company_id = '{{ company_id }}'
+            AND platform = 'tb'
+        )
+        ORDER BY day, platform, shop_id, snick, cnick, msg_time ASC
+    ) AS msg_info
+    GROUP BY day, platform, shop_id, snick, cnick
+) AS msg_time_info
+ARRAY JOIN
+    msg_time_delta_mins_within_session AS msg_time_delta_min_within_session
+GROUP BY msg_time_delta_min_within_session
+ORDER BY msg_time_delta_min_within_session
