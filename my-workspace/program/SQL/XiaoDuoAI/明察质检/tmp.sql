4d481e7e1ddd044db8f4a0c983d53f598a6a0cf7
TomAndersen <1040994898@qq.com>
2022-10-11 11:48:51 +0800

    update

--- my-workspace/program/SQL/XiaoDuoAI/明察质检/tmp.sql
+++ my-workspace/program/SQL/XiaoDuoAI/明察质检/tmp.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS trino ON CLUSTER cluster_3s_2r
+CREATE DATABASE IF NOT EXISTS trino ON CLUSTER cluster_3s_2r ENGINE=Ordinary
 
-CREATE TABLE trino.xqc_shop_local on cluster cluster_3s_2r
+CREATE TABLE trino.xqc_shop_local ON CLUSTER cluster_3s_2r
 (
     `create_time` String,
     `update_time` String,
@@ -14,16 +14,21 @@ CREATE TABLE trino.xqc_shop_local on cluster cluster_3s_2r
 ENGINE = ReplicatedMergeTree(
     '/clickhouse/{database}/tables/{layer}_{shard}/{table}', '{replica}'
 )
-PARTITION BY `day` 
-ORDER BY (shop_id, cnick_id)
+ORDER BY (company_id, shop_id)
 SETTINGS index_granularity=8192, storage_policy='rr'
 
 
-CREATE TABLE tmp.xqc_shop_all on cluster cluster_3s_2r
-as tmp.xqc_shop_local
-ENGINE = Distributed('cluster_3s_2r', 'tmp', 'xqc_shop_local', rand())
+CREATE TABLE trino.xqc_shop_all ON CLUSTER cluster_3s_2r
+AS trino.xqc_shop_local
+ENGINE = Distributed('cluster_3s_2r', 'trino', 'xqc_shop_local', rand())
 
 
-CREATE TABLE buffer.xqc_tmp_shop_buffer ON CLUSTER cluster_3s_2r
-AS tmp.xqc_shop_all
-ENGINE = Buffer('tmp', 'xqc_shop_all', 16, 15, 35, 81920, 409600, 16777216, 67108864)
+CREATE TABLE buffer.trino_xqc_shop_buffer ON CLUSTER cluster_3s_2r
+AS trino.xqc_shop_all
+ENGINE = Buffer('trino', 'xqc_shop_all', 16, 15, 35, 81920, 409600, 16777216, 67108864)
+
+
+INSERT INTO clickhouse.trino.xqc_shop_all
+SELECT
+    CAST(create_time AS varbinary)
+FROM mongodb.xqc.shop;
