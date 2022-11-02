caebdb05d9fdac940ebce642ede646de30d2e8d9
TomAndersen <1040994898@qq.com>
2022-11-02 09:37:43 +0800

    update

--- my-workspace/program/SQL/XiaoDuoAI/明察质检/tmp1.sql
+++ my-workspace/program/SQL/XiaoDuoAI/明察质检/tmp1.sql
@@ -1,15 +1,11 @@
-SELECT ask_count AS hot,
-    question as question_string,
-    t1.question_type AS question_type,
-    t1.question_id,
-    subcategory_name AS category,
-    msg_examples AS example_string
-FROM xd_stat.presale_day_platform_snick_goods_question AS t1
-    JOIN xd_stat.presale_day_platform_snick_goods_question_msg_example_distinct_by_week AS t2 ON t1.question_id = t2.question_id
-WHERE t1.day between 20221022 and 20221028
-    AND t1.snick_oid = '62aa7ddc1aa2d00017dcd4db'
-    AND t1.plat_goods_id = '10059256512910'
-    AND t2.day = 20221028
-    AND t2.snick_oid = '62aa7ddc1aa2d00017dcd4db'
-    AND t2.plat_goods_id = '10059256512910'
-ORDER BY ask_count DESC;
+docker cp 7a6838cfcc45:/etc/hosts ./7a6838cfcc45_hosts
+docker exec -it --user root 7a6838cfcc45 bash -c "echo '10.22.133.216 znzjk-133216-prod-mini-bigdata-bigdata' >> /etc/hosts"
+
+
+
+wget znzjk-113174-prod-mini-bigdata-bigdata:19000
+wget znzjk-113175-prod-mini-bigdata-bigdata:19000
+wget znzjk-133216-prod-mini-bigdata-bigdata:19000
+wget v1mini-bigdata-002:19000
+wget v1mini-bigdata-003:19000
+wget mini-bigdata-004:19000
