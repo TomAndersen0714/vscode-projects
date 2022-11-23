89ba345f53ed91d80e1984424e5d7efe73b697dc
TomAndersen <1040994898@qq.com>
2022-11-23 10:08:23 +0800

    update

--- my-workspace/program/SQL/XiaoDuoAI/明察质检/tmp1.sql
+++ my-workspace/program/SQL/XiaoDuoAI/明察质检/tmp1.sql
@@ -1,18 +1,44 @@
-CREATE TABLE xqc_dim.snick_full_info_local (
-    `company_id` String,
-    `platform` String,
-    `shop_id` String,
-    `department_id` String,
-    `department_name` String,
-    `snick` String,
-    `employee_id` String,
-    `employee_name` String,
-    `superior_id` String,
-    `superior_name` String,
-    `day` Int32
-) ENGINE = ReplicatedMergeTree(
-    '/clickhouse/{database}/tables/{layer}_{shard}/{table}',
-    '{replica}'
-) PARTITION BY day
-ORDER BY (company_id, platform) SETTINGS index_granularity = 8192,
-    storage_policy = 'rr'
+-- ks
+Airflow Web UI:
+https://airflow2.xiaoduoai.com/connection/list/
+
+xdqc_mongo:
+dds-k2je4842705df3e41.mongodb.zhangbei.rds.aliyuncs.com:3717, dds-k2je4842705df3e42.mongodb.zhangbei.rds.aliyuncs.com:3717/admin?replicaSet=mgset-500111166
+xdqc_mongo_ks:
+dds-k2je4842705df3e41.mongodb.zhangbei.rds.aliyuncs.com:3717, dds-k2je4842705df3e42.mongodb.zhangbei.rds.aliyuncs.com:3717/admin?replicaSet=mgset-500111166
+
+scripts:
+1. xqc_etl_ks.py
+2. xqc_message_etl_ks.py
+
+-- dy
+Airflow Web UI:
+https://dy-airflow-v1.xiaoduoai.com/admin/connection/
+
+xdqc_mongo:
+dds-k2je4842705df3e41.mongodb.zhangbei.rds.aliyuncs.com:3717, dds-k2je4842705df3e42.mongodb.zhangbei.rds.aliyuncs.com:3717/admin?replicaSet=mgset-500111166
+xdqc_mongo_dy:
+dds-k2je4842705df3e41.mongodb.zhangbei.rds.aliyuncs.com:3717, dds-k2je4842705df3e42.mongodb.zhangbei.rds.aliyuncs.com:3717/admin?replicaSet=mgset-500111166
+
+scripts:
+1. xqc_etl_dy.py
+2. xqc_message_etl_dy.py
+
+-- tb
+Airflow Web UI:
+https://airflow2.xiaoduoai.com/connection/list/
+
+xdqc_mongo:
+dds-k2je4842705df3e42.mongodb.zhangbei.rds.aliyuncs.com
+
+xdqc_mongo_tb:
+dds-k2je4842705df3e41.mongodb.zhangbei.rds.aliyuncs.com:3717,dds-k2je4842705df3e42.mongodb.zhangbei.rds.aliyuncs.com:3717/admin?replicaSet=mgset-500111166
+
+xdqc_offline:
+dds-k2je4842705df3e41.mongodb.zhangbei.rds.aliyuncs.com:3717,dds-k2je4842705df3e42.mongodb.zhangbei.rds.aliyuncs.com:3717/admin?replicaSet=mgset-500111166
+
+scripts:
+1. message.py
+2. xqc_dim_etl_tb.py
+3. xqc_ods_message_etl_tb.py
+3. xqc_ods_etl_tb.py
