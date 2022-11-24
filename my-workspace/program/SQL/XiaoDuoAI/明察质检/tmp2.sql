4797b929fec6041670fb5aa4924ea6832e5a9b85
TomAndersen <1040994898@qq.com>
2022-11-24 09:44:52 +0800

    update

--- my-workspace/program/SQL/XiaoDuoAI/明察质检/tmp2.sql
+++ my-workspace/program/SQL/XiaoDuoAI/明察质检/tmp2.sql
@@ -1,23 +1,4 @@
-CREATE TABLE xqc_dim.snick_full_info_local (
-    `company_id` String,
-    `company_name` String,
-    `company_short_name` String,
-    `platform` String,
-    `shop_id` String,
-    `shop_name` String,
-    `seller_nick` String,
-    `department_id` String,
-    `department_name` String,
-    `snick` String,
-    `employee_id` String,
-    `employee_name` String,
-    `superior_id` String,
-    `superior_name` String,
-    `day` Int32
-) ENGINE = ReplicatedMergeTree(
-    '/clickhouse/{database}/tables/{layer}_{shard}/{table}',
-    '{replica}'
-)
-PARTITION BY day
-ORDER BY (company_id, platform) SETTINGS index_granularity = 8192,
-    storage_policy = 'rr'
+高级告警比例
+中级告警比例
+初级告警比例
+高级告警完结量
