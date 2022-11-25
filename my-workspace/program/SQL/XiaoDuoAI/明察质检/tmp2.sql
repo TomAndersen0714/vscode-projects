2167dcb6a464836fed33c3f69ce68520becb9609
TomAndersen <1040994898@qq.com>
2022-11-25 23:03:09 +0800

    update

--- my-workspace/program/SQL/XiaoDuoAI/明察质检/tmp2.sql
+++ my-workspace/program/SQL/XiaoDuoAI/明察质检/tmp2.sql
@@ -17,3 +17,12 @@ AND seller_nick = '方太官方旗舰店'
 AND last_mark_id != ''
 GROUP BY toYYYYMMDD(begin_time) AS day
 ORDER BY day
+
+-- 测试用例3: 对比会话总量和已检会话量
+SELECT day, COUNT(1), SUM(last_mark_id!='')
+FROM dwd.xdqc_dialog_all
+WHERE toYYYYMMDD(begin_time) BETWEEN toYYYYMMDD(toDate('{{date.start}}'))
+    AND toYYYYMMDD(toDate('{{date.end}}'))
+AND seller_nick = '木月瑞希尔'
+GROUP BY toYYYYMMDD(begin_time) AS day
+ORDER BY day
