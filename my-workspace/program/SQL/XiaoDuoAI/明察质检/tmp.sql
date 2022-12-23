abae2484305620defa463c36f2fc07f34598746c
TomAndersen <1040994898@qq.com>
2022-12-23 11:56:40 +0800

    update

--- my-workspace/program/SQL/XiaoDuoAI/明察质检/tmp.sql
+++ my-workspace/program/SQL/XiaoDuoAI/明察质检/tmp.sql
@@ -1,46 +1,19 @@
-
-        SELECT
-            sum((100 - score + score_add) >= toUInt8OrZero('{{ passing_score=100 }}')) AS qualified_dialog_sum
-        FROM dwd.xdqc_dialog_all
-        WHERE toYYYYMMDD(begin_time) BETWEEN toYYYYMMDD(toDate('{{ day.start }}'))
-            AND toYYYYMMDD(toDate('{{ day.end }}'))
-        -- 筛选指定平台
-        AND platform = 'tb'
-        -- 筛选指定企业的店铺
-        AND seller_nick IN (
-            SELECT DISTINCT
-                seller_nick
-            FROM xqc_dim.xqc_shop_all
-            WHERE day = toYYYYMMDD(yesterday())
-            -- 筛选指定企业
-            AND company_id = '{{ company_id=5f747ba42c90fd0001254404 }}'
-            -- 筛选指定平台
-            AND platform = 'tb'
-            -- 下拉框-店铺主账号
-            AND (
-                '{{ seller_nicks }}'=' '
-                OR
-                seller_nick IN splitByChar(',', '{{ seller_nicks }}')
-            )
-        )
-        -- 筛选指定质检标准对应的子账号
-        AND (
-            '{{ qc_norm_ids }}'=' '
-            OR
-            snick GLOBAL IN (
-                -- 筛选指定子账号分组中的子账号
-                SELECT snick
-                FROM xqc_dim.snick_full_info_all
-                WHERE day = toYYYYMMDD(yesterday())
-                AND company_id = '{{ company_id=5f747ba42c90fd0001254404 }}'
-                AND department_id IN (
-                    -- 筛选指定质检标准对应的子账号分组
-                    SELECT department_id
-                    FROM ods.xinghuan_qc_norm_relate_all
-                    WHERE day = toYYYYMMDD(yesterday())
-                    AND qc_norm_id IN splitByChar(',', '{{ qc_norm_ids }}')
-                    AND company_id = '{{ company_id=5f747ba42c90fd0001254404 }}'
-                )
-            )
-        )
-    
+upsert into ods.goods (
+    platform,
+    plat_goods_id,
+    shop_id,
+    _id,
+    create_time,
+    plat_goods_name,
+    plat_goods_url,
+    plat_goods_img
+)
+select 'jd' as platform,
+    plat_goods_id,
+    shop_id,
+    goods_id,
+    create_time,
+    plat_goods_name,
+    plat_goods_url,
+    plat_goods_img
+from tmp.jd_goods
