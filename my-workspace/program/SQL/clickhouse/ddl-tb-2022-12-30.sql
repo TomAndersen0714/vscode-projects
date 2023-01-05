9f319177f78c196f11995226c73e611f9c921f81
TomAndersen <1040994898@qq.com>
2023-01-05 15:18:22 +0800

    update

--- my-workspace/program/SQL/clickhouse/ddl-tb-2022-12-30.sql
+++ my-workspace/program/SQL/clickhouse/ddl-tb-2022-12-30.sql
@@ -94,22 +94,22 @@ ADD COLUMN IF NOT EXISTS `send_msg_from` Int64 AFTER `is_first_msg_within_sessio
 
 -- ft_dwd.session_detail_all
 ALTER TABLE ft_dwd.session_detail_local ON CLUSTER cluster_3s_2r
-ADD COLUMN `m_session_send_cnt` Int64 AFTER `session_send_cnt`,
-ADD COLUMN `qa_cnt` Int64 AFTER `m_session_send_cnt`,
-ADD COLUMN `qa_reply_intervals_secs` Array(Int64) AFTER `qa_cnt`,
-ADD COLUMN `m_qa_cnt` Int64 AFTER `qa_reply_intervals_secs`,
-ADD COLUMN `m_qa_reply_intervals_secs` Array(Int64) AFTER `m_qa_cnt`;
+ADD COLUMN IF NOT EXISTS `m_session_send_cnt` Int64 AFTER `session_send_cnt`,
+ADD COLUMN IF NOT EXISTS `qa_cnt` Int64 AFTER `m_session_send_cnt`,
+ADD COLUMN IF NOT EXISTS `qa_reply_intervals_secs` Array(Int64) AFTER `qa_cnt`,
+ADD COLUMN IF NOT EXISTS `m_qa_cnt` Int64 AFTER `qa_reply_intervals_secs`,
+ADD COLUMN IF NOT EXISTS `m_qa_reply_intervals_secs` Array(Int64) AFTER `m_qa_cnt`;
 
 ALTER TABLE ft_dwd.session_detail_all ON CLUSTER cluster_3s_2r
-ADD COLUMN `m_session_send_cnt` Int64 AFTER `session_send_cnt`,
-ADD COLUMN `qa_cnt` Int64 AFTER `m_session_send_cnt`,
-ADD COLUMN `qa_reply_intervals_secs` Array(Int64) AFTER `qa_cnt`,
-ADD COLUMN `m_qa_cnt` Int64 AFTER `qa_reply_intervals_secs`,
-ADD COLUMN `m_qa_reply_intervals_secs` Array(Int64) AFTER `m_qa_cnt`;
+ADD COLUMN IF NOT EXISTS `m_session_send_cnt` Int64 AFTER `session_send_cnt`,
+ADD COLUMN IF NOT EXISTS `qa_cnt` Int64 AFTER `m_session_send_cnt`,
+ADD COLUMN IF NOT EXISTS `qa_reply_intervals_secs` Array(Int64) AFTER `qa_cnt`,
+ADD COLUMN IF NOT EXISTS `m_qa_cnt` Int64 AFTER `qa_reply_intervals_secs`,
+ADD COLUMN IF NOT EXISTS `m_qa_reply_intervals_secs` Array(Int64) AFTER `m_qa_cnt`;
 
 ALTER TABLE buffer.ft_dwd_session_detail_buffer ON CLUSTER cluster_3s_2r
-ADD COLUMN `m_session_send_cnt` Int64 AFTER `session_send_cnt`,
-ADD COLUMN `qa_cnt` Int64 AFTER `m_session_send_cnt`,
-ADD COLUMN `qa_reply_intervals_secs` Array(Int64) AFTER `qa_cnt`,
-ADD COLUMN `m_qa_cnt` Int64 AFTER `qa_reply_intervals_secs`,
-ADD COLUMN `m_qa_reply_intervals_secs` Array(Int64) AFTER `m_qa_cnt`;
+ADD COLUMN IF NOT EXISTS `m_session_send_cnt` Int64 AFTER `session_send_cnt`,
+ADD COLUMN IF NOT EXISTS `qa_cnt` Int64 AFTER `m_session_send_cnt`,
+ADD COLUMN IF NOT EXISTS `qa_reply_intervals_secs` Array(Int64) AFTER `qa_cnt`,
+ADD COLUMN IF NOT EXISTS `m_qa_cnt` Int64 AFTER `qa_reply_intervals_secs`,
+ADD COLUMN IF NOT EXISTS `m_qa_reply_intervals_secs` Array(Int64) AFTER `m_qa_cnt`;
