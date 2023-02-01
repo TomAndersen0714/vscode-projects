5cd8c1305d46ad6b06fc637a2e30e2eba0ecaac7
TomAndersen <1040994898@qq.com>
2023-02-01 20:01:27 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp.sql
@@ -1,291 +1,37 @@
-select shop_id,
-    `day`,
-    snick,
-    goods_id,
-    "付款量",
-    "出库量",
-    "付款人数",
-    "出库人数",
-    round(
-        (
-            if(
-                isNull("付款量")
-                or "付款量" = 0,
-                0,
-                "出库量" / "付款量"
-            )
-        ) * 100,
-        2
-    ) as "出库率（量）",
-    round(
-        (
-            if(
-                isNull("付款人数")
-                or "付款人数" = 0,
-                0,
-                "出库人数" / "付款人数"
-            )
-        ) * 100,
-        2
-    ) as "出库率（人数）"
-from (
-        --出库量，付款量
-        select shop_id,
-            `day`,
-            snick,
-            goods_id,
-            "付款量",
-            "出库量"
-        from (
-                select shop_id,
-                    `day`,
-                    snick,
-                    goods_id,
-                    sum(goods_count) as "付款量"
-                from (
-                        SELECT shop_id,
-                            `day`,
-                            snick,
-                            goods_id,
-                            goods_num,
-                            count(1) * goods_num as goods_count
-                        FROM (
-                                SELECT DISTINCT shop_id,
-                                    `day`,
-                                    order_id,
-                                    goods_id,
-                                    goods_num
-                                FROM ft_dwd.order_detail_all
-                                WHERE shop_id = '5cac112e98ef4100118a9c9f'
-                                    AND `day` = 20220909
-                                    AND status IN ('paid', 'deposited')
-                                    AND goods_id GLOBAL NOT IN (
-                                        SELECT goods_id
-                                        FROM ft_dim.goods_info_all
-                                    )
-                            )
-                            LEFT JOIN (
-                                SELECT DISTINCT shop_id,
-                                    `day`,
-                                    order_id,
-                                    snick
-                                FROM ft_dwd.ask_order_cov_detail_all
-                                WHERE shop_id = '5cac112e98ef4100118a9c9f'
-                                    AND `day` >= 2022098
-                                    AND `day` <= 20220909
-                                    AND cycle = '2'
-                                    AND paid_time != ''
-                            ) USING shop_id,
-                            `day`,
-                            order_id
-                        where (
-                                snick != ''
-                                OR snick IS NULL
-                            )
-                        group by shop_id,
-                            `day`,
-                            snick,
-                            goods_id,
-                            goods_num
+-- 顾客产出价值衡量
+-- 店铺*客服粒度-N件销售人数分布-销售
+SELECT `day`,
+    shop_id,
+    '{{platform}}' AS platform,
+    sales_count,
+    'buyer_distribution' AS stat_label,
+    count(DISTINCT real_buyer_nick) AS stat_value,
+    toString(now64(3, 'Asia/Shanghai')) AS update_at
+FROM (
+        SELECT `day`,
+            shop_id,
+            real_buyer_nick,
+            sum(goods_num) AS sales_count
+        FROM ft_dwd.order_detail_all
+        WHERE shop_id = '{{shop_id}}'
+            AND `day` = { { ds_nodash } }
+            AND status IN ('paid', 'deposited') --付定金的商品也算在内
+            AND goods_id GLOBAL NOT IN (
+                SELECT DISTINCT goods_id
+                FROM ft_dim.goods_info_all
+                WHERE shop_id = '{{shop_id}}'
+                    AND `type` IN (
+                        '1',
+                        '2',
+                        '3',
+                        '4'
                     )
-                group by shop_id,
-                    `day`,
-                    snick,
-                    goods_id
             )
-            full outer join (
-                select shop_id,
-                    `day`,
-                    snick,
-                    goods_id,
-                    sum(goods_count) as "出库量"
-                from (
-                        SELECT shop_id,
-                            `day`,
-                            snick,
-                            goods_id,
-                            goods_num,
-                            count(1) * goods_num as goods_count
-                        FROM (
-                                SELECT DISTINCT shop_id,
-                                    `day`,
-                                    order_id,
-                                    goods_id,
-                                    goods_num
-                                FROM ft_dwd.order_detail_all
-                                WHERE shop_id = '5cac112e98ef4100118a9c9f'
-                                    AND `day` = 20220909
-                                    AND status IN ('shipped')
-                                    AND goods_id GLOBAL NOT IN (
-                                        SELECT goods_id
-                                        FROM ft_dim.goods_info_all
-                                    )
-                            )
-                            LEFT JOIN (
-                                SELECT DISTINCT shop_id,
-                                    `day`,
-                                    order_id,
-                                    snick
-                                FROM ft_dwd.ask_order_cov_detail_all
-                                WHERE shop_id = '5cac112e98ef4100118a9c9f'
-                                    AND `day` >= 2022098
-                                    AND `day` <= 20220909
-                                    AND cycle = '2'
-                                    AND paid_time != ''
-                            ) USING shop_id,
-                            `day`,
-                            order_id
-                        where (
-                                snick != ''
-                                OR snick IS NULL
-                            )
-                        group by shop_id,
-                            `day`,
-                            snick,
-                            goods_id,
-                            goods_num
-                    )
-                group by shop_id,
-                    `day`,
-                    snick,
-                    goods_id
-            ) using shop_id,
-            `day`,
-            snick,
-            goods_id
+        GROUP BY `day`,
+            shop_id,
+            real_buyer_nick
     )
-    full outer join (
-        --出库人数/付款人数
-        select shop_id,
-            `day`,
-            snick,
-            goods_id,
-            "付款人数",
-            "出库人数"
-        from (
-                --付款人数
-                select shop_id,
-                    `day`,
-                    snick,
-                    goods_id,
-                    count(distinct buyer_nick) as "付款人数"
-                from (
-                        select shop_id,
-                            `day`,
-                            buyer_nick,
-                            snick,
-                            goods_id,
-                            goods_num,
-                            status_arr
-                        from (
-                                select shop_id,
-                                    `day`,
-                                    order_id,
-                                    goods_id,
-                                    goods_num,
-                                    buyer_nick,
-                                    groupArray(status) as status_arr
-                                from (
-                                        SELECT distinct shop_id,
-                                            `day`,
-                                            order_id,
-                                            buyer_nick,
-                                            goods_id,
-                                            goods_num,
-                                            status
-                                        FROM ft_dwd.order_detail_all
-                                        WHERE shop_id = '5cac112e98ef4100118a9c9f'
-                                            AND `day` = 20220909
-                                    )
-                                group by shop_id,
-                                    `day`,
-                                    order_id,
-                                    goods_id,
-                                    goods_num,
-                                    buyer_nick
-                            )
-                            left join (
-                                select distinct shop_id,
-                                    `day`,
-                                    order_id,
-                                    snick,
-                                    goods_id
-                                from ft_dwd.ask_order_cov_detail_all
-                                WHERE shop_id = '5cac112e98ef4100118a9c9f'
-                                    and `day` >= 20220908
-                                    AND `day` <= 20220909
-                                    and cycle = '2'
-                                    and paid_time != ''
-                            ) using (shop_id, `day`, order_id)
-                        where snick != ''
-                    )
-                where has(status_arr, 'paid')
-                group by shop_id,
-                    `day`,
-                    snick,
-                    goods_id
-            )
-            full outer join (
-                --出库人数
-                select shop_id,
-                    `day`,
-                    snick,
-                    goods_id,
-                    count(distinct buyer_nick) as "出库人数"
-                from (
-                        select shop_id,
-                            `day`,
-                            buyer_nick,
-                            snick,
-                            goods_id,
-                            goods_num,
-                            status_arr
-                        from (
-                                select shop_id,
-                                    `day`,
-                                    order_id,
-                                    goods_id,
-                                    goods_num,
-                                    buyer_nick,
-                                    groupArray(status) as status_arr
-                                from (
-                                        SELECT distinct shop_id,
-                                            `day`,
-                                            order_id,
-                                            buyer_nick,
-                                            goods_id,
-                                            goods_num,
-                                            status
-                                        FROM ft_dwd.order_detail_all
-                                        WHERE shop_id = '5cac112e98ef4100118a9c9f'
-                                            AND `day` = 20220909
-                                    )
-                                group by shop_id,
-                                    `day`,
-                                    order_id,
-                                    goods_id,
-                                    goods_num,
-                                    buyer_nick
-                            )
-                            left join (
-                                select distinct shop_id,
-                                    `day`,
-                                    order_id,
-                                    snick,
-                                    goods_id
-                                from ft_dwd.ask_order_cov_detail_all
-                                WHERE shop_id = '5cac112e98ef4100118a9c9f'
-                                    and `day` >= 20220908
-                                    AND `day` <= 20220909
-                                    and cycle = '2'
-                                    and paid_time != ''
-                            ) using (shop_id, `day`, order_id)
-                        where snick != ''
-                    )
-                where has(status_arr, 'shipped')
-                group by shop_id,
-                    `day`,
-                    snick,
-                    goods_id
-            ) using (shop_id, `day`, snick, goods_id)
-    ) using (shop_id, `day`, snick, goods_id)
+GROUP BY `day`,
+    shop_id,
+    platform,
+    sales_count
--- my-workspace/project/xiaoduo/明察质检/tmp1.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp1.sql
@@ -1,310 +1,38 @@
--- 店铺*客服*产品粒度-出库量（不含静默）
--- 店铺*客服*产品粒度-付款量（不含静默）
--- 店铺*客服*产品粒度-出库率（出库量 / 付款量）（不含静默）
--- 店铺*客服*产品粒度-出库人数（不含静默）
--- 店铺*客服*产品粒度-付款人数（不含静默）
--- 店铺*客服*产品粒度-出库率（出库人数 / 付款人数）（不含静默）
-SELECT
-    `day`,
+SELECT `day`,
     shop_id,
-    '{{platform}}' as platform,
+    '{{platform}}' AS platform,
     snick,
-    goods_id,
-    stat_label,
-    stat_value,
+    'all' AS goods_id,
+    'out_of_stock_amt' AS stat_label,
+    toFloat64(sum(goods_payment)) AS stat_value,
     toString(now64(3, 'Asia/Shanghai')) as update_at
 FROM (
-    select `day`,
-        shop_id,
-        snick,
-        goods_id,
-        ask_order_paid_cnt,
-        ask_order_paid_uv,
-        out_of_stock_cnt,
-        out_of_stock_uv,
-        round(
-            (
-                if(
-                    isNull(ask_order_paid_cnt)
-                    or ask_order_paid_cnt = 0,
-                    0,
-                    out_of_stock_cnt / ask_order_paid_cnt
-                )
-            ) * 100,
-            2
-        ) as out_of_stock_cnt_rat,
-        round(
-            (
-                if(
-                    isNull(ask_order_paid_uv)
-                    or ask_order_paid_uv = 0,
-                    0,
-                    out_of_stock_uv / ask_order_paid_uv
-                )
-            ) * 100,
-            2
-        ) as out_of_stock_uv_rat
-    from (
-        select shop_id,
-            `day`,
-            snick,
+    SELECT *
+    FROM (
+        SELECT shop_id,
+            toYYYYMMDD(toDate('{{ds}}')) AS `day`,
+            buyer_nick as cnick,
+            order_id,
             goods_id,
-            ask_order_paid_cnt,
-            out_of_stock_cnt
-        from (
-                select shop_id,
-                    `day`,
-                    snick,
-                    goods_id,
-                    sum(goods_count) as ask_order_paid_cnt
-                from (
-                        SELECT shop_id,
-                            `day`,
-                            snick,
-                            goods_id,
-                            goods_num,
-                            count(1) * goods_num as goods_count
-                        FROM (
-                            SELECT DISTINCT shop_id,
-                                `day`,
-                                order_id,
-                                goods_id,
-                                goods_num
-                            FROM ft_dwd.order_detail_all
-                            WHERE shop_id = '{{shop_id}}'
-                                AND `day` = toYYYYMMDD(subtractDays(toDate('{{ds}}'),{{cycle}} - 1))
-                                AND status IN ('paid', 'deposited')
-                                AND goods_id GLOBAL NOT IN (
-                                    SELECT goods_id
-                                    FROM ft_dim.goods_info_all
-                                )
-                        )
-                        LEFT JOIN (
-                                    SELECT DISTINCT shop_id,
-                                        `day`,
-                                        order_id,
-                                        snick
-                                    FROM ft_dwd.ask_order_cov_detail_all
-                                    WHERE shop_id = '{{shop_id}}'
-                                        AND `day` = toYYYYMMDD(subtractDays(toDate('{{ds}}'),{{cycle}} - 1))
-                                        AND cycle = {{cycle}}
-                                        AND paid_time != ''
-                        )
-                        USING shop_id,
-                            `day`,
-                            order_id
-                        where (
-                                    snick != ''
-                                    OR snick IS NULL
-                                )
-                        group by shop_id,
-                            `day`,
-                            snick,
-                            goods_id,
-                            goods_num
-                )
-                group by shop_id,
-                    `day`,
-                    snick,
-                    goods_id
-        )
-        full outer join (
-                select shop_id,
-                    `day`,
-                    snick,
-                    goods_id,
-                    sum(goods_count) as out_of_stock_cnt
-                from (
-                    SELECT shop_id,
-                        `day`,
-                        snick,
-                        goods_id,
-                        goods_num,
-                        count(1) * goods_num as goods_count
-                    FROM (
-                            SELECT DISTINCT shop_id,
-                                `day`,
-                                order_id,
-                                goods_id,
-                                goods_num
-                            FROM ft_dwd.order_detail_all
-                            WHERE shop_id = '{{shop_id}}'
-                                AND `day` = toYYYYMMDD(subtractDays(toDate('{{ds}}'),{{cycle}} - 1))
-                                AND status IN ('shipped')
-                                AND goods_id GLOBAL NOT IN (
-                                    SELECT goods_id
-                                    FROM ft_dim.goods_info_all
-                                )
-                    )
-                    LEFT JOIN (
-                            SELECT DISTINCT shop_id,
-                                `day`,
-                                order_id,
-                                snick
-                            FROM ft_dwd.ask_order_cov_detail_all
-                            WHERE shop_id = '{{shop_id}}'
-                                AND `day` = toYYYYMMDD(subtractDays(toDate('{{ds}}'),{{cycle}} - 1))
-                                AND cycle = {{cycle}}
-                                AND paid_time != ''
-                    )
-                    USING shop_id,
-                        `day`,
-                        order_id
-                    where (
-                            snick != ''
-                            OR snick IS NULL
-                        )
-                    group by shop_id,
-                        `day`,
-                        snick,
-                        goods_id,
-                        goods_num
-                )
-                group by shop_id,
-                    `day`,
-                    snick,
-                    goods_id
-        )
-        using shop_id,
-            `day`,
-            snick,
-            goods_id
+            goods_payment,
+            goods_num
+        FROM ft_dwd.order_detail_all
+        WHERE `day` <= toYYYYMMDD(addDays(toDate('{{ds}}'), {{cycle}} - 1))
+            AND `day` >= toYYYYMMDD(toDate('{{ds}}'))
+            AND shop_id = '{{shop_id}}'
+            AND status IN ('shipped')
     )
-    full outer join (
-        select shop_id,
-            `day`,
-            snick,
-            goods_id,
-            ask_order_paid_uv,
-            out_of_stock_uv
-        from (
-            select shop_id,
-                `day`,
-                snick,
-                goods_id,
-                count(distinct buyer_nick) as ask_order_paid_uv
-            from (
-                    select shop_id,
-                        `day`,
-                        buyer_nick,
-                        snick,
-                        goods_id,
-                        goods_num,
-                        status_arr
-                    from (
-                            select shop_id,
-                                `day`,
-                                order_id,
-                                goods_id,
-                                goods_num,
-                                buyer_nick,
-                                groupArray(status) as status_arr
-                            from (
-                                SELECT distinct shop_id,
-                                    `day`,
-                                    order_id,
-                                    buyer_nick,
-                                    goods_id,
-                                    goods_num,
-                                    status
-                                FROM ft_dwd.order_detail_all
-                                WHERE shop_id = '{{shop_id}}'
-                                    AND `day` = toYYYYMMDD(subtractDays(toDate('{{ds}}'),{{cycle}} - 1))
-                            )
-                            group by shop_id,
-                                `day`,
-                                order_id,
-                                goods_id,
-                                goods_num,
-                                buyer_nick
-                    )
-                    left join (
-                            select distinct shop_id,
-                                `day`,
-                                order_id,
-                                snick,
-                                goods_id
-                            from ft_dwd.ask_order_cov_detail_all
-                            WHERE shop_id = '{{shop_id}}'
-                                and `day` = toYYYYMMDD(subtractDays(toDate('{{ds}}'),{{cycle}} - 1))
-                                and cycle = {{cycle}}
-                                and paid_time != ''
-                    ) using (shop_id, `day`, order_id)
-                    where snick != ''
-            )
-            where has(status_arr, 'paid')
-            group by shop_id,
-                `day`,
-                snick,
-                goods_id
-        )
-        full outer join (
-            select shop_id,
-                `day`,
-                snick,
-                goods_id,
-                count(distinct buyer_nick) as out_of_stock_uv
-            from (
-                select shop_id,
-                    `day`,
-                    buyer_nick,
-                    snick,
-                    goods_id,
-                    goods_num,
-                    status_arr
-                from (
-                        select shop_id,
-                            `day`,
-                            order_id,
-                            goods_id,
-                            goods_num,
-                            buyer_nick,
-                            groupArray(status) as status_arr
-                        from (
-                            SELECT distinct shop_id,
-                                `day`,
-                                order_id,
-                                buyer_nick,
-                                goods_id,
-                                goods_num,
-                                status
-                            FROM ft_dwd.order_detail_all
-                            WHERE shop_id = '{{shop_id}}'
-                                AND `day` = toYYYYMMDD(subtractDays(toDate('{{ds}}'),{{cycle}} - 1))
-                        )
-                        group by shop_id,
-                            `day`,
-                            order_id,
-                            goods_id,
-                            goods_num,
-                            buyer_nick
-                    )
-                    left join (
-                        select distinct shop_id,
-                            `day`,
-                            order_id,
-                            snick,
-                            goods_id
-                        from ft_dwd.ask_order_cov_detail_all
-                        WHERE shop_id = '{{shop_id}}'
-                            and `day` = toYYYYMMDD(subtractDays(toDate('{{ds}}'),{{cycle}} - 1))
-                            and cycle = {{cycle}}
-                            and paid_time != ''
-                    ) using (shop_id, `day`, order_id)
-                where snick != ''
-            )
-            where has(status_arr, 'shipped')
-            group by shop_id,
-                `day`,
-                snick,
-                goods_id
-        )
-        using (shop_id, `day`, snick, goods_id)
+    JOIN (
+        SELECT DISTINCT order_id,
+            snick
+        FROM ft_dwd.ask_order_cov_detail_all
+        WHERE `day` = toYYYYMMDD(toDate('{{ds}}'))
+            AND cycle = {{cycle}}
+            AND paid_time != ''
+            AND shop_id = '{{shop_id}}'
     )
-    using (shop_id, `day`, snick, goods_id)
+    USING order_id
 )
-ARRAY JOIN
-    ['{{cycle}}_ask_order_paid_cnt','{{cycle}}_out_of_stock_cnt','{{cycle}}_out_of_stock_cnt_rat',
-     '{{cycle}}_ask_order_paid_order_uv','{{cycle}}_out_of_stock_uv','{{cycle}}_out_of_stock_uv_rat'] AS stat_label,
-     [ask_order_paid_cnt, out_of_stock_cnt, out_of_stock_cnt_rat,
-      ask_order_paid_uv, out_of_stock_uv, out_of_stock_uv_rat] AS stat_value;
+GROUP BY shop_id,
+    `day`,
+    snick
