e1ba661d3299ff2e08e5b5f493b993af9ebecea8
TomAndersen <1040994898@qq.com>
2023-03-07 20:00:24 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp3.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp3.sql
@@ -1,255 +1,41 @@
-{
-    "http_endpoint": ":8155",
-    "grpc_endpoint": ":8156",
-    "pprof_endpoint": ":8157",
-    "stream_step_size": 1000,
-    "std_log": {
-        "level": "info",
-        "file": "/var/log/xiaoduo/query-sdk.log",
-        "err_file": "/var/log/xiaoduo/query-sdk.err.log",
-        "app_name": "query-sdk",
-        "auto_init": true
-    },
-    "api_log": {
-        "level": "info",
-        "file": "/var/log/xiaoduo/query-sdk.api.log",
-        "app_name": "query-sdk",
-        "auto_init": true
-    },
-    "switcher": ["10.20.0.125:2379"],
-    "rate_limit": {
-        "default": {
-            "impala": {
-                "qpm": 600,
-                "max": 10
-            },
-            "clickhouse": {
-                "qpm": 600,
-                "max": 20
-            },
-            "mysql": {
-                "qpm": 600,
-                "max": 10
-            },
-            "postgres":{
-                "qpm": 1200,
-                "qps": 20,
-                "max": 10
-            }
-        },
-        "config-check": {
-            "impala": {
-                "qpm": 1200,
-                "max": 20
-            },
-            "clickhouse": {
-                "qpm": 1200,
-                "max": 20
-            },
-            "mysql": {
-                "qpm": 600,
-                "max": 10
-            },
-            "postgres":{
-                "qpm": 1200,
-                "qps": 20,
-                "max": 10
-            }
-        },
-        "data-visual":{
-            "impala": {
-                "qpm": 600,
-                "max": 10
-            },
-            "clickhouse": {
-                "qpm": 1200,
-                "max": 41
-            },
-            "mysql": {
-                "qpm": 600,
-                "max": 10
-            }
-        },
-        "csm":{
-            "impala": {
-                "qpm": 240,
-                "max": 4
-            },
-            "clickhouse": {
-                "qpm": 240,
-                "max": 4
-            },
-            "mysql": {
-                "qpm": 240,
-                "max": 4
-            }
-        },  
-        "snowball":{
-            "impala": {
-                "qpm": 2,
-                "max": 1
-            },
-            "clickhouse": {
-                "qpm": 2,
-                "max": 1
-            },
-            "mysql": {
-                "qpm": 100,
-                "max": 20
-            }
-        },      
-        "x-data-x-data": {
-            "impala": {
-                "qpm": 300,
-                "max": 5
-            },
-            "clickhouse": {
-                "qpm": 1200,
-                "max": 20
-            },
-            "mysql": {
-                "qpm": 600,
-                "max": 10
-            }
-        },
-        "x-data-screen": {
-            "impala": {
-                "qpm": 300,
-                "max": 5
-            },
-            "clickhouse": {
-                "qpm": 1200,
-                "max": 20
-            },
-            "mysql": {
-                "qpm": 600,
-                "max": 10
-            }
-        },
-        "x-data-shandian": {
-            "impala": {
-                "qpm": 300,
-                "max": 5
-            },
-            "clickhouse": {
-                "qpm": 600,
-                "max": 10
-            },
-            "mysql": {
-                "qpm": 600,
-                "max": 10
-            }
-        },
-        "x-data-xcm": {
-            "impala": {
-                "qpm": 300,
-                "max": 5
-            },
-            "clickhouse": {
-                "qpm": 1200,
-                "max": 20
-            },
-            "mysql": {
-                "qpm": 600,
-                "max": 10
-            }
-        },
-        "x-data-xz": {
-            "impala": {
-                "qpm": 300,
-                "max": 5
-            },
-            "clickhouse": {
-                "qpm": 1200,
-                "max": 20
-            },
-            "mysql": {
-                "qpm": 600,
-                "max": 10
-            }
-        },
-        "x-data-jd": {
-            "impala": {
-                "qpm": 300,
-                "max": 5
-            },
-            "clickhouse": {
-                "qpm": 1800,
-                "max": 30
-            }
-        },
-        "x-data-csm": {
-            "impala": {
-                "qpm": 300,
-                "max": 5
-            },
-            "clickhouse": {
-                "qpm": 1200,
-                "max": 20
-            },
-            "mysql": {
-                "qpm": 600,
-                "max": 10
-            }
-        },
-        "x-data-x-data-trans": {
-            "impala": {
-                "qpm": 300,
-                "max": 5
-            },
-            "clickhouse": {
-                "qpm": 1200,
-                "max": 20
-            },
-            "mysql": {
-                "qpm": 600,
-                "max": 10
-            }
-        },
-        "x-data-xqc": {
-            "impala": {
-                "qpm": 600,
-                "max": 5
-            },
-            "clickhouse": {
-                "qps": 900,
-                "max": 10
-            }
-        },
-        "keban-group": {
-            "impala": {
-                "qpm": 10, 
-                "max": 3
-            },
-            "clickhouse": {
-                "qpm": 10,
-                "max": 10
-            }
-        }
-    },
-    "storage": {
-        "ping": "20s",
-        "impala": {
-            "default": ["10.20.2.29:21050","10.20.133.176:21050","10.20.133.149:21050"],
-            "bee": ["10.20.2.29:21000","10.20.133.176:21000","10.20.133.149:21000"]
-        },
-        "clickhouse": {
-            "default": ["tcp://10.20.2.29:19000?check_connection_liveness=true&read_timeout=300&alt_hosts=10.20.133.149:19000&max_memory_usage=20000000000","tcp://10.20.133.149:19000?read_timeout=300&alt_hosts=10.20.2.29:19000&max_memory_usage=20000000000"],
-            "mp": ["tcp://10.20.2.29:19000?check_connection_liveness=true&read_timeout=300&alt_hosts=10.20.133.149:19000&max_memory_usage=20000000000","tcp://10.20.133.149:19000?read_timeout=300&alt_hosts=10.20.2.29:19000&max_memory_usage=20000000000"],
-            "keban": ["tcp://10.20.133.173:19000?debug=false"],
-            "log": ["tcp://10.20.131.58:9000?debug=false", "tcp://10.20.131.14:9000?debug=false"]
-        },
-        "mysql" : {
-            "default": ["select.user:hT0KABSS*X@tcp(172.16.124.5:3306)/jira?charset=utf8&parseTime=true"],
-            "snowball": ["root:Mysql1234!@tcp(10.20.0.173:3306)/snowball?charset=utf8&parseTime=true"]
-        },
-        "postgres": {
-			"default": [
-			    "host=10.20.133.149 port=5432 user=postgres dbname=mayfly password=mysecretpassword sslmode=disable TimeZone=Asia/Shanghai"
-			],
-			"goodscenter": [
-				"host=pgm-k2jkzdt0rm806b04112900.pgsql.zhangbei.rds.aliyuncs.com port=5432 user=barracks dbname=barracks password=BaxSn2T$o89NJFs sslmode=disable TimeZone=Asia/Shanghai"
-			]
-        }
-    }
-}
+CREATE DATABASE IF NOT EXISTS ods ON CLUSTER cluster_3s_2r
+ENGINE=Ordinary;
+
+-- DROP TABLE ods.jd_real_time_chat_log_local ON CLUSTER cluster_3s_2r NO DELAY
+CREATE TABLE IF NOT EXISTS ods.jd_real_time_chat_log_local ON CLUSTER cluster_3s_2r
+(
+    `shop_id` String,
+    `plat_user_id` String,
+    `main_username` String,
+    `mt` Int64,
+    `channel` String,
+    `waiter` String,
+    `time` Int64,
+    `type` String,
+    `waiter_send` String,
+    `content` String,
+    `sid` String,
+    `customer` String,
+    `sku_id` Int64,
+    `day` Int32
+)
+ENGINE = ReplicatedMergeTree(
+    '/clickhouse/{database}/tables/{layer}_{shard}/{table}',
+    '{replica}'
+)
+PARTITION BY day
+ORDER BY (shop_id, waiter)
+SETTINGS index_granularity = 8192, storage_policy = 'rr';
+
+-- DROP TABLE ods.jd_real_time_chat_log_all ON CLUSTER cluster_3s_2r NO DELAY
+CREATE TABLE IF NOT EXISTS ods.jd_real_time_chat_log_all ON CLUSTER cluster_3s_2r
+AS ods.jd_real_time_chat_log_local
+ENGINE = Distributed('cluster_3s_2r', 'ods', 'jd_real_time_chat_log_local', rand());
+
+CREATE DATABASE IF NOT EXISTS buffer ON CLUSTER cluster_3s_2r
+ENGINE=Ordinary;
+
+-- DROP TABLE buffer.ods_jd_real_time_chat_log_buffer ON CLUSTER cluster_3s_2r NO DELAY
+CREATE TABLE IF NOT EXISTS buffer.ods_jd_real_time_chat_log_buffer ON CLUSTER cluster_3s_2r
+AS ods.jd_real_time_chat_log_all
+ENGINE = Buffer('ods', 'jd_real_time_chat_log_all', 16, 15, 35, 81920, 409600, 16777216, 67108864);
