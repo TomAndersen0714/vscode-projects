7643f92841174611e613a3838155812584c801a0
TomAndersen <1040994898@qq.com>
2023-03-31 18:32:12 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp3.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp3.sql
@@ -1,18 +1,118 @@
-select msg_time,
-	if (
-		real_buyer_nick is not null
-		and real_buyer_nick != '',
-		real_buyer_nick,
-		cnick
-	) as cnick,
-	snick,
-	msg,
-	answer_id,
-	act,
-	send_msg_from,
-	is_withdraw
-from dwd.withdraw_context
-where query_id = '64183f4afd1ac200015aa8a0'
-order by msg_time
+SELECT question_id,
+       question AS `咨询场景`,
+       rank1 AS `排名`,
+       uniqCnt AS `人数`,
+       dialog_cnt AS `声量`,
+       cn_change AS `排名变化`
+FROM
+  (SELECT question_id,
+          uniqCnt,
+          dialog_cnt,
+          rank1,
+          rank2-rank1 AS rank_change,
+          IF (rank2=0
+              OR rank_change=0,
+                 '-',
+                 IF (rank_change>0,
+                     concat('上升',toString(rank_change)),
+                     concat('下降',toString(abs(rank_change))))) AS cn_change
+   FROM
+     (SELECT question_id,
+             uniqCnt,
+             dialog_cnt,
+             rowNumberInAllBlocks()+1 AS rank1
+      FROM
+        (SELECT question_id,
+                groupBitmapOr(cnick_id_bitmap) AS uniqCnt,
+                sum(dialog_sum) AS dialog_cnt
+         FROM
+           (SELECT *
+            FROM dws.voc_goods_question_stat_all
+         WHERE day BETWEEN toYYYYMMDD(toDate('{{ day.start=week_ago }}')) AND toYYYYMMDD(toDate('{{ day.end=yesterday }}'))
+           AND platform = 'tb' -- 下拉店铺
 
+           AND question_id!=''
+           AND question_id GLOBAL IN
+             (SELECT DISTINCT qid AS question_id
+              FROM dim.question_b_v2_all
+              WHERE third_category_id IN ['6045c92df9fbadde8fcca533','616932c3980a37bdebf19a6e','60785d962648234a2b7bc9dc'] )
+           AND ('{{ shop_ids }}'=''
+                OR shop_id IN splitByChar(',','{{ shop_ids }}') )-- 下拉订单状态
+-- 下拉订单状态
+AND ('{{ order_status }}'=''
+     OR recent_order_status IN splitByChar(',',replaceAll('{{ order_status }}', 'unorder', '')))
+           AND ('{{ round_count }}'=''
+                OR toString(dialog_qa_stage) IN splitByChar(',','{{ round_count }}'))-- 当前企业对应的店铺
+AND shop_id GLOBAL IN
+             (SELECT DISTINCT shop_id
+              FROM xqc_dim.xqc_shop_all
+              WHERE day = toYYYYMMDD(yesterday())
+                AND company_id = '{{ company_id=63fc50f0a06a5ecd9a249ac9 }}'
+                AND platform = 'tb' )-- 当前企业对应的子账号
+AND snick GLOBAL IN
+             (SELECT DISTINCT snick
+              FROM ods.xinghuan_employee_snick_all
+              WHERE day = toYYYYMMDD(yesterday())
+                AND platform = 'tb'
+                AND company_id = '{{ company_id=63fc50f0a06a5ecd9a249ac9 }}' -- 下拉框-子账号分组id
 
+                AND ('{{ department_ids }}'=''
+                     OR department_id IN splitByChar(',','{{ department_ids }}') )-- 下拉子账号
+
+                AND ('{{ snicks }}'=''
+                     OR snick IN splitByChar(',','{{ snicks }}')) )
+)
+         GROUP BY question_id
+         ORDER BY uniqCnt DESC
+         LIMIT 10)) GLOBAL
+   LEFT JOIN
+     (SELECT question_id,
+             uniqCnt2,
+             rowNumberInAllBlocks()+1 AS rank2
+      FROM
+        (SELECT question_id,
+                groupBitmapOr(cnick_id_bitmap) AS uniqCnt2
+         FROM
+           (SELECT *
+            FROM dws.voc_goods_question_stat_all
+         WHERE day BETWEEN toYYYYMMDD(subtractDays(toDate('{{ day.start=week_ago }}'),dateDiff('day', toDate('{{ day.start=week_ago }}'),toDate('{{ day.end=yesterday }}'))+1)) AND toYYYYMMDD(subtractDays(toDate('{{ day.start=week_ago }}'),1))
+           AND platform = 'tb' -- 下拉店铺
+AND ('{{ shop_ids }}'=''
+     OR shop_id IN splitByChar(',','{{ shop_ids }}') )-- 下拉订单状态
+-- 下拉订单状态
+AND ('{{ order_status }}'=''
+     OR recent_order_status IN splitByChar(',',replaceAll('{{ order_status }}', 'unorder', '')))
+           AND ('{{ round_count }}'=''
+                OR toString(dialog_qa_stage) IN splitByChar(',','{{ round_count }}') )-- 当前企业对应的店铺
+
+           AND question_id!=''
+           AND question_id GLOBAL IN
+             (SELECT DISTINCT qid AS question_id
+              FROM dim.question_b_v2_all
+              WHERE third_category_id IN ['6045c92df9fbadde8fcca533','616932c3980a37bdebf19a6e','60785d962648234a2b7bc9dc'] )
+           AND shop_id GLOBAL IN
+             (SELECT DISTINCT shop_id
+              FROM xqc_dim.xqc_shop_all
+              WHERE day = toYYYYMMDD(yesterday())
+                AND company_id = '{{ company_id=63fc50f0a06a5ecd9a249ac9 }}'
+                AND platform = 'tb' )-- 当前企业对应的子账号
+AND snick GLOBAL IN
+             (SELECT DISTINCT snick
+              FROM ods.xinghuan_employee_snick_all
+              WHERE day = toYYYYMMDD(yesterday())
+                AND platform = 'tb'
+                AND company_id = '{{ company_id=63fc50f0a06a5ecd9a249ac9 }}' -- 下拉框-子账号分组id
+
+                AND ('{{ department_ids }}'=''
+                     OR department_id IN splitByChar(',','{{ department_ids }}') )-- 下拉子账号
+
+                AND ('{{ snicks }}'=''
+                     OR snick IN splitByChar(',','{{ snicks }}')) )
+)
+         GROUP BY question_id
+         ORDER BY uniqCnt2 DESC)) USING(question_id)) AS rank_info GLOBAL
+LEFT JOIN
+  (SELECT qid,
+          question
+   FROM dim.question_b_v2_all
+   WHERE third_category_id IN ['6045c92df9fbadde8fcca533','616932c3980a37bdebf19a6e','60785d962648234a2b7bc9dc'] ) AS qb ON rank_info.question_id = qb.qid
--- my-workspace/project/xiaoduo/明察质检/tmp6.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp6.sql
@@ -1,44 +1,142 @@
+INSERT INTO dwd.voc_chat_log_detail_all
 SELECT
     day,
     platform,
     shop_id,
     snick,
-    question_id,
-    dialog_qa_stage,
-    dialog_goods_id,
-    recent_order_id,
-    recent_order_status,
-    recent_order_status_timestamp,
-    groupBitmapState(cnick_id) AS cnick_id_bitmap,
-    bitmapCardinality(cnick_id_bitmap) AS dialog_sum
+    cnick,
+    cnick_id,
+    real_buyer_nick,
+    msg_timestamp,
+    msg_id,
+    msg,
+    act,
+    send_msg_from,
+    question_b_qid,
+    plat_goods_id,
+    IF(recent_order_status!='', latest_order_info.order_id, '') AS recent_order_id,
+    arrayFilter(
+        (x)-> x<=msg_timestamp,
+        latest_order_info.order_status_timestamps
+    )[-1] AS recent_order_status_timestamp,
+    arrayFilter(
+        (x,y)-> y<=msg_timestamp,
+        latest_order_info.order_statuses,
+        latest_order_info.order_status_timestamps
+    )[-1] AS recent_order_status,
+    dialog_qa_cnt AS dialog_qa_sum
 FROM (
     SELECT
         day,
         platform,
         shop_id,
         snick,
-        question_b_qid AS question_id,
-        CASE
-            WHEN dialog_qa_sum=0 THEN 0
-            WHEN dialog_qa_sum>0 AND dialog_qa_sum<=3 THEN 1
-            WHEN dialog_qa_sum>3 AND dialog_qa_sum<=10 THEN 2
-            ELSE 3
-        END AS dialog_qa_stage,
-        plat_goods_id AS dialog_goods_id,
-        recent_order_id,
-        recent_order_status,
-        recent_order_status_timestamp
-    FROM dwd.voc_chat_log_detail_all
-    WHERE day = {{ds_nodash}}
-    AND shop_id IN {{VOC_SHOP_IDS}}
-)
-GROUP BY day,
-    platform,
-    shop_id,
-    snick,
-    question_id,
-    dialog_qa_stage,
-    dialog_goods_id,
-    recent_order_id,
-    recent_order_status,
-    recent_order_status_timestamp
+        cnick,
+        cnick_id,
+        real_buyer_nick,
+        msg_timestamp,
+        msg_id,
+        msg,
+        act,
+        send_msg_from,
+        question_b_qid,
+        plat_goods_id,
+        dialog_detail_info.dialog_qa_cnt
+    FROM (
+        -- stage_1, 获取当天的聊天消息记录
+        SELECT
+            day,
+            platform,
+            shop_id,
+            replaceOne(snick,'cntaobao','') AS snick,
+            replaceOne(cnick,'cntaobao','') AS cnick,
+            '' AS real_buyer_nick,
+            toUInt64(msg_time) AS msg_timestamp,
+            msg_id,
+            msg,
+            act,
+            send_msg_from,
+            question_b_qid,
+            plat_goods_id
+        FROM ods.xdrs_logs_all
+        WHERE day = 20230330
+        AND shop_id IN ['60b72d421edc070017428380']
+        AND act IN ['send_msg', 'recv_msg']
+    ) AS xdrs_logs
+    LEFT JOIN (
+        SELECT
+            day,
+            platform,
+            shop_id,
+            snick,
+            cnick,
+            cnick_info.cnick_id,
+            real_buyer_nick,
+            dialog_qa_cnt
+        FROM (
+            -- stage_2, 使用当天的聊天消息计算会话轮次, 按照会话聚合, 统计会话轮次
+            SELECT
+                day,
+                platform,
+                shop_id,
+                snick,
+                cnick,
+                real_buyer_nick,
+                arraySort(groupArray(msg_milli_timestamp)) AS msg_milli_timestamps,
+                arraySort((x, y)->y, groupArray(act), groupArray(msg_milli_timestamp)) AS msg_acts,
+
+                -- 切分会话生成QA切分标记, PS: 可能存在单个Q, 单个A, 单个QA, 多个QA四种情况, 此切分方法只能切分多QA的情况
+                arrayMap(
+                    (x, y)->(if(x = 'send_msg' AND msg_acts[y-1] = 'recv_msg', 1, 0)),
+                    msg_acts,
+                    arrayEnumerate(msg_acts)
+                ) AS _qa_split_tags,
+                -- QA数量
+                arraySum(_qa_split_tags) AS dialog_qa_cnt
+            FROM (
+                SELECT
+                    day,
+                    platform,
+                    shop_id,
+                    replaceOne(snick,'cntaobao','') AS snick,
+                    replaceOne(cnick,'cntaobao','') AS cnick,
+                    '' AS real_buyer_nick,
+                    toUInt64(toFloat64(toDateTime64(create_time, 3))*1000) AS msg_milli_timestamp,
+                    act
+                FROM ods.xdrs_logs_all
+                WHERE day = 20230330
+                AND shop_id IN ['60b72d421edc070017428380']
+                AND act IN ['send_msg', 'recv_msg']
+            ) AS xdrs_logs
+            GROUP BY day,
+                platform,
+                shop_id,
+                snick,
+                cnick,
+                real_buyer_nick
+        ) AS dialog_info
+        LEFT JOIN (
+            SELECT
+                cnick,
+                cnick_id
+            FROM dwd.voc_cnick_list_all
+            WHERE day = 20230330
+            AND platform = 'tb'
+        ) AS cnick_info
+        USING(cnick)
+    ) AS dialog_detail_info
+    USING(day, platform, shop_id, snick, cnick, real_buyer_nick)
+) AS xdrs_dialog_info
+LEFT JOIN (
+    -- stage_4, 关联买家最新订单表, 获取订单状态
+    SELECT
+        day,
+        buyer_nick AS cnick,
+        order_id,
+        order_status_timestamps,
+        order_statuses
+    FROM dwd.voc_buyer_latest_order_all
+    WHERE day = 20230330
+    AND shop_id IN ['60b72d421edc070017428380']
+) AS latest_order_info
+USING(day, cnick)
