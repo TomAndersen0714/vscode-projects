6c66a562c43d934a968de88aea98f0eaa128728e
TomAndersen <1040994898@qq.com>
2023-04-07 21:52:29 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp5.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp5.sql
@@ -1,24 +1,84 @@
-select t1.buyer_nick,
-    created_day
-from (
-        select buyer_nick,
-            created_day
-        from ft_dwd.new_ask_order_cov_buyer_detail_all
-        where cycle = 2
-            and shop_id = '5cac112e98ef4100118a9c9f' -- and act='paid'
-            -- and created_day<=20230401
-            and update_time <= '2023-03-21' -- and buyer_nick like '%one%'
-            and paid_time <> ''
-    ) as t1
-    left join (
-        select distinct buyer_nick
-        from ft_dwd.new_ask_order_cov_buyer_snick_detail_all
-        where cycle = 2
-            and shop_id = '5cac112e98ef4100118a9c9f'
-            and act = 'paid' -- and snick='方太官方旗舰店:七七'
-            and buyer_nick like '%one%'
-            and created_day <= 20230319
-    ) as t2 on t1.buyer_nick = t2.buyer_nick
-where t2.buyer_nick = ''
-order by created_day desc
-limit 1000
+SELECT `day`,
+    shop_id,
+    '{{platform}}' AS platform,
+    snick,
+    'all' as goods_id,
+    stat_label,
+    stat_value,
+    toString(now64(3, 'Asia/Shanghai')) as update_at
+FROM (
+        select shop_id,
+            `day`,
+            snick,
+            sum(payment) as collocation_amt,
+            --2+x搭配额
+            count(distinct cnick) as collocation_cnt --2+x搭配量
+        from (
+                SELECT shop_id,
+                    `day`,
+                    snick,
+                    cnick,
+                    groupArray(order_id) as order_ids,
+                    groupArray(goods_id) as goods_ids,
+                    arraySum(groupArray(goods_payment)) as payment,
+                    length(arrayDistinct(groupArray(tag))) as tags_cnt
+                FROM (
+                        SELECT *
+                        FROM (
+                                SELECT *,
+                                    if(
+                                        sku_info.sku_id != '',
+                                        0,
+                                        1
+                                    ) AS tag
+                                FROM (
+                                        SELECT shop_id,
+                                            buyer_nick AS cnick,
+                                            order_id,
+                                            goods_id,
+                                            goods_payment
+                                        FROM ft_dwd.order_detail_all
+                                        WHERE `day` <= toYYYYMMDD(addDays(toDate('{{ds}}'), { { cycle } } - 1))
+                                            AND `day` >= toYYYYMMDD(toDate('{{ds}}'))
+                                            AND shop_id = '{{shop_id}}'
+                                            AND status IN ('shipped')
+                                            AND goods_id GLOBAL NOT IN (
+                                                SELECT goods_id
+                                                FROM ft_dim.goods_info_all
+                                                WHERE `type` IN (
+                                                        '1',
+                                                        '2',
+                                                        '3',
+                                                        '4'
+                                                    )
+                                            )
+                                    ) AS order_info
+                                    LEFT JOIN (
+                                        SELECT DISTINCT sku_id
+                                        FROM ft_dim.main_goods_info_all
+                                        WHERE product_class IN ('油烟机', '灶具')
+                                    ) AS sku_info ON order_info.goods_id = sku_info.sku_id
+                            )
+                            JOIN (
+                                SELECT DISTINCT `day`,
+                                    order_id,
+                                    snick
+                                FROM ft_dwd.ask_order_cov_detail_all
+                                WHERE `day` = toYYYYMMDD(toDate('{{ds}}'))
+                                    AND cycle = { { cycle } }
+                                    AND paid_time != ''
+                                    AND shop_id = '{{shop_id}}'
+                            ) USING order_id
+                    )
+                GROUP BY shop_id,
+                    `day`,
+                    snick,
+                    cnick
+                having tags_cnt > 1
+            )
+        group by shop_id,
+            `day`,
+            snick
+    ) ARRAY
+    JOIN ['{{cycle}}_out_of_stock_collocation_amt', '{{cycle}}_out_of_stock_paid_collocation_cnt'] AS stat_label,
+    [toString(collocation_amt), toString(collocation_cnt)] AS stat_value;
---
+++ my-workspace/project/xiaoduo/明察质检/tmp6.sql
@@ -0,0 +1,69 @@
+SELECT
+    `day`,
+    shop_id,
+    'jd' AS platform,
+    'all' as goods_id,
+    stat_label,
+    stat_value,
+    toString(now64(3, 'Asia/Shanghai')) as update_at
+FROM (
+    SELECT `day`,
+        shop_id,
+        sum(payment) AS collocation_amt, --2+x搭配额
+        count(distinct buyer_nick) AS collocation_cnt --2+x搭配量
+    FROM (
+            SELECT shop_id,
+                `day`,
+                buyer_nick,
+                groupArray(order_id) AS order_ids,
+                groupArray(goods_id) AS goods_ids,
+                arraySum(groupArray(goods_payment)) AS payment,
+                length(arrayDistinct(groupArray(tag))) AS tags_cnt
+            FROM (
+                    SELECT
+                        *,
+                        if(
+                            sku_info.sku_id != '',
+                            0,
+                            1
+                        ) AS tag
+                    FROM (
+                        SELECT shop_id,
+                            `day`,
+                            buyer_nick,
+                            order_id,
+                            goods_id,
+                            goods_payment
+                        FROM ft_dwd.order_detail_all
+                        WHERE `day` = toYYYYMMDD(toDate('2023-04-06'))
+                            AND shop_id = '5edfa47c8f591c00163ef7d6'
+                            AND status IN ('paid', 'deposited')
+                            AND goods_id GLOBAL NOT IN (
+                                SELECT goods_id
+                                FROM ft_dim.goods_info_all
+                                WHERE `type` IN (
+                                        '1',
+                                        '2',
+                                        '3',
+                                        '4'
+                                    )
+                            )
+                    ) AS order_info
+                    LEFT JOIN (
+                        SELECT DISTINCT sku_id
+                        FROM ft_dim.main_goods_info_all
+                        WHERE product_class IN ('油烟机', '灶具')
+                    ) AS sku_info
+                    ON order_info.goods_id = sku_info.sku_id
+                )
+            GROUP BY shop_id,
+                `day`,
+                buyer_nick
+            having tags_cnt > 1
+        )
+    group by shop_id,
+        `day`
+)
+ARRAY JOIN
+    ['paid_collocation_amt', 'paid_collocation_cnt'] AS stat_label,
+    [toString(collocation_amt), toString(collocation_cnt)] AS stat_value
