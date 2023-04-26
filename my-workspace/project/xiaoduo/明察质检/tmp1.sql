ba23f92c294533c56bcb2a4597f3e8c545f59c7b
TomAndersen <1040994898@qq.com>
2023-04-26 20:00:49 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp1.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp1.sql
@@ -1,124 +1,14 @@
-INSERT INTO { { sink_table } } 
-SELECT 
-  company_id, 
-  question_b_qid, 
-  question_b_name, 
-  group_id, 
-  group_name, 
-  group_level, 
-  parent_group_id, 
-  parent_group_name, 
-  first_group_id, 
-  first_group_name, 
-  second_group_id, 
-  second_group_name, 
-  third_group_id, 
-  third_group_name, 
-  fourth_group_id, 
-  fourth_group_name, 
-  create_time, 
-  update_time 
-FROM 
-  (
-    SELECT 
-      company_id, 
-      question_b_qid, 
-      question_b_name, 
-      group_id, 
-      create_time, 
-      update_time 
-    FROM 
-      (
-        SELECT 
-          company_id, 
-          name AS question_b_name, 
-          group_id, 
-          create_time, 
-          update_time 
-        FROM 
-          dim.voc_question_b_all 
-        WHERE 
-          company_id IN [ '5cac112e98ef4100118a9c9f' ]
-      ) AS voc_question_info 
-      INNER JOIN (
-        -- 获取企业店铺行业场景
-        SELECT 
-          company_id, 
-          shop_id, 
-          subcategory_id, 
-          question_b_qid, 
-          question_b_name 
-        FROM 
-          (
-            -- 获取企业店铺行业场景一级分组, PS: category_id:subcategory_id=N:N
-            SELECT 
-              DISTINCT company_id, 
-              shop_id, 
-              subcategory_id 
-            FROM 
-              (
-                -- 获取企业店铺非专属品类
-                SELECT 
-                  company_id, 
-                  shop_id, 
-                  category_id 
-                FROM 
-                  (
-                    SELECT 
-                      _id AS shop_id, 
-                      category_id 
-                    FROM 
-                      dim.xdre_shop_all 
-                    WHERE 
-                      _id IN [ '5cac112e98ef4100118a9c9f' ] -- 筛选非专属模型店铺
-                      AND model_type ! = '1'
-                  ) AS shop_category_info GLOBAL 
-                  INNER JOIN (
-                    SELECT 
-                      company_id, 
-                      shop_id 
-                    FROM 
-                      numbers(1) ARRAY 
-                      JOIN [ '5cac112e98ef4100118a9c9f' ] AS company_id, 
-                      [ '5cac112e98ef4100118a9c9f' ] AS shop_id
-                  ) AS voc_shop_info USING(shop_id)
-              ) AS company_shop_category_info GLOBAL 
-              INNER JOIN (
-                SELECT 
-                  DISTINCT category_id, 
-                  subcategory_id 
-                FROM 
-                  dim.category_subcategory_all
-              ) AS cate_map_info USING(category_id)
-          ) AS company_shop_subcategory_info 
-          INNER JOIN (
-            SELECT 
-              qid AS question_b_qid, 
-              question AS question_b_name, 
-              subcategory_id 
-            FROM 
-              dim.question_b_v2_all
-          ) AS question_b_info USING(subcategory_id)
-      ) AS robot_question_info USING(company_id, question_b_name)
-  ) 
-  LEFT JOIN (
-    SELECT 
-      company_id, 
-      group_id, 
-      group_name, 
-      group_level, 
-      parent_group_id, 
-      parent_group_name, 
-      first_group_id, 
-      first_group_name, 
-      second_group_id, 
-      second_group_name, 
-      third_group_id, 
-      third_group_name, 
-      fourth_group_id, 
-      fourth_group_name 
-    FROM 
-      dim.voc_question_b_group_detail_all 
-    WHERE 
-      company_id IN [ '5cac112e98ef4100118a9c9f' ]
-  ) AS company_group_info USING(company_id, group_id)
+CREATE TABLE xqc_dim.xqc_shop_all
+(
+    `_id` String,
+    `create_time` String,
+    `update_time` String,
+    `company_id` String,
+    `shop_id` String,
+    `platform` String,
+    `seller_nick` String,
+    `plat_shop_name` String,
+    `plat_shop_id` String,
+    `day` Int32
+)
+ENGINE = Distributed('cluster_3s_2r', 'xqc_dim', 'xqc_shop_local', rand())
