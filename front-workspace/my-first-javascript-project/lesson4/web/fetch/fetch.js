b5e79232083e0e76fcf22d1653b1e26bc8fa6957
TomAndersen <1040994898@qq.com>
2023-05-04 00:26:23 +0800

    update

---
+++ front-workspace/my-first-javascript-project/lesson4/web/fetch/fetch.js
@@ -0,0 +1,2 @@
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Fetching_data
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
---
+++ front-workspace/my-first-javascript-project/lesson4/web/references.md
@@ -0,0 +1,2 @@
+# MDN - Web API
+https://developer.mozilla.org/en-US/docs/Web/API
