46ea2cf35fc54134e61845e8aa24f1366d766b48
TomAndersen <1040994898@qq.com>
2023-05-05 00:36:45 +0800

    update

--- front-workspace/my-first-javascript-project/lesson4/network/xhr/xhr-api.js
+++ front-workspace/my-first-javascript-project/lesson4/network/xhr/xhr-api.js
@@ -3,7 +3,9 @@ https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/F
 https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
 https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/HTML_in_XMLHttpRequest
 https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+http://www.ruanyifeng.com/blog/2012/09/xmlhttprequest_level_2.html
 */
+// NOTE: XMLHttpRequest===XHR
 
 // NOTE: XMLHttpRequest instance properties
 // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest#instance_properties
