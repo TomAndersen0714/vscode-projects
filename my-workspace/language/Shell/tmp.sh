c0d79f18089c50956107c23022b24b754b780ddc
TomAndersen <1040994898@qq.com>
2023-05-22 20:00:42 +0800

    update

--- my-workspace/language/Shell/tmp.sh
+++ my-workspace/language/Shell/tmp.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
-rm -rfv test1 test2 test1~
-mkdir -pv test1 test2/test1
-touch test1/txt1 test2/test1/txt2-1
+curl 'https://wangcai-dev-lane.xiaoduoai.com/api/query-panel/v1/sql-builder/execute?id=643d3124e2fd7f045782ed0a' \
+  -H 'sec-ch-ua: "Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"' \
+  -H 'sec-ch-ua-platform: "Windows"' \
+  -H 'Referer: https://dataforce.xiaoduoai.com/' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36' \
+  -H 'Content-Type: application/json' \
+  --data-raw '{"status":2,"id":"643d3124e2fd7f045782ed0a","inputs":{},"external_type":0}' \
+  --compressed
