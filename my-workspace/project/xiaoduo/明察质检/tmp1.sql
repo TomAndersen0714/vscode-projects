605dd80fb1a1f51036f94c0a186dfa9b2a6409e4
TomAndersen <1040994898@qq.com>
2023-06-03 03:27:21 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp1.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp1.sql
@@ -1,36 +1,73 @@
-_id
-String
-qid
-String
-question
-String
-subcategory_id
-String
-tags
-String
-answers
-String
-is_transfer
-String
-is_dynamicable
-String
-is_dynamic
-String
-is_editable
-String
-auto_send_in_hybrid_mode
-String
-auto_send_in_auto_mode
-String
-replies
-String
-create_time
-String
-update_time
-String
-subcategory_ids
-Array(String)
-question_sample
-Array(String)
-sid
-String
+SELECT
+    dialog_info.begin_time AS `会话时间`,
+    snick_depart_info.department_name AS `子账号分组`,
+    dialog_info.snick AS `子账号`,
+    snick_depart_info.employee_name AS `客服姓名`,
+    dialog_info.real_buyer_nick AS `顾客姓名`,
+    dialog_info.order_id AS `订单号`,
+    multiIf(
+       dialog_info.order_status = '', '',
+       dialog_info.order_status = 'created', '已创建',
+       dialog_info.order_status = 'deposited', '付定金',
+       dialog_info.order_status = 'paid', '已付款',
+       dialog_info.order_status = 'shipped', '已发货',
+       dialog_info.order_status = 'succeeded', '已成功',
+       dialog_info.order_status = 'closed', '已关闭',
+       ''
+    ) AS `订单状态`,
+    order_time AS `订单时间`,
+    order_payment AS `订单金额`,
+    focus_goods_id AS `焦点商品`
+FROM (
+    SELECT
+        _id AS dialog_id,
+        begin_time,
+        snick,
+        real_buyer_nick,
+        order_info_status[1] AS order_status,
+        order_info_id[1] AS order_id,
+        order_info_time[1] AS order_time,
+        order_info_payment[1] AS order_payment,
+        focus_goods_id
+    FROM dwd.xdqc_dialog_all
+    WHERE toYYYYMMDD(begin_time) BETWEEN toYYYYMMDD(toDate('{{ day.start }}'))
+        AND toYYYYMMDD(toDate('{{ day.end }}'))
+    AND platform = 'tb'
+    -- 在会话表里，筛选会话轮次
+    AND qa_round_sum >= 1
+    AND seller_nick IN ['顾家家居旗舰店', 'kuka顾家家居旗舰店']
+    AND _id GLOBAL IN (
+        SELECT DISTINCT
+            dialog_id
+        FROM xqc_ods.message_all
+        WHERE day BETWEEN toYYYYMMDD(toDate('{{ day.start }}'))
+            AND toYYYYMMDD(toDate('{{ day.end }}'))
+        AND platform = 'tb'
+        AND seller_nick IN ['顾家家居旗舰店', 'kuka顾家家居旗舰店']
+        -- 筛选枚举值：客服发送消息1，买家发送消息0
+        AND source = 1
+        -- 筛选指定焦点商品
+        -- AND plat_goods_id = '682312202291'
+        -- 包含指定关键字
+        AND (
+            content LIKE '%企微%' OR
+            content LIKE '%企业微信%' OR
+            content LIKE '%香薰片' OR
+            content LIKE '%VX%' OR
+            content LIKE '%企业VX%' OR
+            content LIKE '%微信%' OR
+            content LIKE '%工作号%' OR
+            content LIKE '%工作微信%' OR
+            content LIKE '%vx%'
+        )
+        -- AND content LIKE '%682312202291%'
+    )
+) AS dialog_info
+GLOBAL LEFT JOIN (
+    SELECT department_name, snick, employee_name
+    FROM xqc_dim.snick_full_info_all
+    WHERE day = toYYYYMMDD(yesterday())
+    AND company_id = '614d86d84eed94e6fc980b1c'
+    AND seller_nick IN ['顾家家居旗舰店', 'kuka顾家家居旗舰店']
+) AS snick_depart_info
+USING(snick)
--- my-workspace/project/xiaoduo/明察质检/tmp2.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp2.sql
@@ -1,32 +1,74 @@
-_id
-String
-qid
-String
-question
-String
-subcategory_id
-String
-tags
-String
-answers
-String
-is_transfer
-String
-is_dynamicable
-String
-is_dynamic
-String
-is_editable
-String
-auto_send_in_hybrid_mode
-String
-auto_send_in_auto_mode
-String
-replies
-String
-create_time
-String
-update_time
-String
-subcategory_ids
-Array(String)
+SELECT *
+FROM (
+    SELECT
+        shop_id, snick, cnick, 
+        msg_time, msg,
+        IF(msg='send_msg', concat(snick, ':\n', msg), concat(cnick, ':\n', msg)) AS content
+    FROM ods.xdrs_logs_all
+    -- 筛选指定时间范围(强制限制近3天)
+    WHERE day BETWEEN toYYYYMMDD(toDate('{{end_date}}') - 3)
+        AND toYYYYMMDD(toDate('{{end_date}}'))
+    -- 筛选普通消息
+    AND act IN ['send_msg', 'recv_msg']
+    -- 筛选指定店铺
+    AND shop_id GLOBAL IN (
+        SELECT
+            shop_id
+        FROM xqc_dim.xqc_shop_all
+        WHERE day = toYYYYMMDD(yesterday())
+        -- 筛选指定平台
+        AND platform = '{{platform}}'
+        -- 筛选指定店铺-主账号
+        AND seller_nick = '{{seller_nick}}'
+    )
+    -- 筛选流失客户
+    AND cnick GLOBAL IN (
+        -- 筛选x天内, 发生过y次会话, 未下单, 买家信息
+        SELECT
+            concat('cntaobao', cnick) AS plat_cnick
+        FROM (
+            SELECT
+                platform,
+                seller_nick,
+                cnick,
+                COUNT(DISTINCT _id) AS dialog_cnt
+            FROM dwd.xdqc_dialog_all
+                -- 筛选指定时间范围(强制限制近3天)
+            WHERE toYYYYMMDD(begin_time) BETWEEN toYYYYMMDD(toDate('{{end_date}}') -3)
+                AND toYYYYMMDD(toDate('{{end_date}}'))
+                -- 筛选指定平台
+                AND platform = '{{platform}}'
+                -- 筛选指定店铺-主账号
+                AND seller_nick = '{{seller_nick}}'
+                -- 筛选最近未下单买家, 即剔除已下单买家
+                AND cnick GLOBAL NOT IN (
+                    SELECT DISTINCT
+                        buyer_nick
+                    FROM ods.order_event_all
+                        -- 筛选指定时间范围
+                    WHERE day BETWEEN toYYYYMMDD(toDate('{{end_date}}') -2)
+                        AND toYYYYMMDD(toDate('{{end_date}}'))
+                        -- 筛选指定店铺
+                        AND shop_id GLOBAL IN (
+                            SELECT
+                                shop_id
+                            FROM xqc_dim.xqc_shop_all
+                            WHERE day = toYYYYMMDD(yesterday())
+                            -- 筛选指定平台
+                            AND platform = '{{platform}}'
+                            -- 筛选指定店铺-主账号
+                            AND seller_nick = '{{seller_nick}}'
+                        )
+                )
+            GROUP BY
+                platform, seller_nick, cnick
+            HAVING
+                -- 筛选会话数量
+                dialog_cnt > toUInt32('{{dialog_cnt}}')
+        ) AS cnick_info
+        -- 限制买家数量
+        LIMIT 50
+    )
+    ORDER BY shop_id, snick, cnick, msg_time
+)
+GROUP BY shop_id, snick, cnick
