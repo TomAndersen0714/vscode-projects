24d769d2ad0d2670f18a02fdde8aa86f1fd58d6a
TomAndersen <1040994898@qq.com>
2023-07-07 18:14:22 +0800

    update

--- my-workspace/language/SQL/ClickHouse/DDL/2023-07-06/2023-07-06-xqc-alert.sql
+++ my-workspace/language/SQL/ClickHouse/DDL/2023-07-06/2023-07-06-xqc-alert.sql
@@ -1,14 +1,40 @@
 -- ALTER TABLE xqc_ods.alert_local ON CLUSTER cluster_3s_2r 
--- DROP COLUMN IF EXISTS `status`;
-ALTER TABLE xqc_ods.alert_local ON CLUSTER cluster_3s_2r
-ADD COLUMN IF NOT EXISTS `status` Int64 AFTER `day`;
+-- DROP COLUMN IF EXISTS `status`,
+-- DROP COLUMN IF EXISTS `handler`,
+-- DROP COLUMN IF EXISTS `note`,
+-- DROP COLUMN IF EXISTS `remind_num`,
+-- DROP COLUMN IF EXISTS `remind_time`;
+ALTER TABLE xqc_ods.alert_local ON CLUSTER cluster_3s_2r 
+ADD COLUMN IF NOT EXISTS `status` Int64 AFTER `is_finished`,
+ADD COLUMN IF NOT EXISTS `handler` String AFTER `superior_name`,
+ADD COLUMN IF NOT EXISTS `note` String AFTER `handler`,
+ADD COLUMN IF NOT EXISTS `remind_num` Int64 AFTER `note`,
+ADD COLUMN IF NOT EXISTS `remind_time` DateTime AFTER `remind_num`;
+
 
 -- ALTER TABLE xqc_ods.alert_all ON CLUSTER cluster_3s_2r
--- DROP COLUMN IF EXISTS `status`;
-ALTER TABLE xqc_ods.alert_all ON CLUSTER cluster_3s_2r
-ADD COLUMN IF NOT EXISTS `status` Int64 AFTER `day`;
+-- DROP COLUMN IF EXISTS `status`,
+-- DROP COLUMN IF EXISTS `handler`,
+-- DROP COLUMN IF EXISTS `note`,
+-- DROP COLUMN IF EXISTS `remind_num`,
+-- DROP COLUMN IF EXISTS `remind_time`;
+ALTER TABLE xqc_ods.alert_all ON CLUSTER cluster_3s_2r 
+ADD COLUMN IF NOT EXISTS `status` Int64 AFTER `is_finished`,
+ADD COLUMN IF NOT EXISTS `handler` String AFTER `superior_name`,
+ADD COLUMN IF NOT EXISTS `note` String AFTER `handler`,
+ADD COLUMN IF NOT EXISTS `remind_num` Int64 AFTER `note`,
+ADD COLUMN IF NOT EXISTS `remind_time` DateTime AFTER `remind_num`;
+
 
 -- ALTER TABLE buffer.xqc_ods_alert_buffer ON CLUSTER cluster_3s_2r
--- DROP COLUMN IF EXISTS `status`;
-ALTER TABLE buffer.xqc_ods_alert_buffer ON CLUSTER cluster_3s_2r
-ADD COLUMN IF NOT EXISTS `status` Int64 AFTER `day`;
+-- DROP COLUMN IF EXISTS `status`,
+-- DROP COLUMN IF EXISTS `handler`,
+-- DROP COLUMN IF EXISTS `note`,
+-- DROP COLUMN IF EXISTS `remind_num`,
+-- DROP COLUMN IF EXISTS `remind_time`;
+ALTER TABLE buffer.xqc_ods_alert_buffer ON CLUSTER cluster_3s_2r 
+ADD COLUMN IF NOT EXISTS `status` Int64 AFTER `is_finished`,
+ADD COLUMN IF NOT EXISTS `handler` String AFTER `superior_name`,
+ADD COLUMN IF NOT EXISTS `note` String AFTER `handler`,
+ADD COLUMN IF NOT EXISTS `remind_num` Int64 AFTER `note`,
+ADD COLUMN IF NOT EXISTS `remind_time` DateTime AFTER `remind_num`;
--- my-workspace/project/xiaoduo/明察质检/tmp.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp.sql
@@ -1,112 +1,109 @@
--- DROP TABLE ods.xdqc_dialog_local ON CLUSTER cluster_3s_2r NO DELAY
-CREATE TABLE ods.xdqc_dialog_local ON CLUSTER cluster_3s_2r
+-- DROP TABLE xqc_ods.alert_local ON CLUSTER cluster_3s_2r NO DELAY;
+CREATE TABLE xqc_ods.alert_local ON CLUSTER cluster_3s_2r
 (
-    `_id` String,
+    `id` String,
     `platform` String,
-    `channel` String,
-    `group` String,
-    `date` Int32,
+    `level` Int64,
+    `warning_type` String,
+    `dialog_id` String,
+    `message_id` String,
+    `time` String,
+    `day` Int64,
+    `is_finished` String,
+    `finish_time` String,
+    `shop_id` String,
     `seller_nick` String,
-    `cnick` String,
-    `real_buyer_nick` String,
-    `open_uid` String,
     `snick` String,
-    `begin_time` DateTime64(3),
-    `end_time` DateTime64(3),
-    `is_after_sale` UInt8,
-    `is_inside` UInt8,
+    `cnick` String,
+    `buyer_one_id` String,
+    `open_uid` String,
     `employee_name` String,
-    `s_emotion_type` Array(UInt16),
-    `s_emotion_rule_id` Array(String),
-    `s_emotion_score` Array(Int32),
-    `s_emotion_count` Array(UInt32),
-    `c_emotion_type` Array(UInt16),
-    `c_emotion_rule_id` Array(String),
-    `c_emotion_score` Array(Int32),
-    `c_emotion_count` Array(UInt32),
-    `emotions` Array(String),
-    `abnormals_type` Array(UInt16),
-    `abnormals_rule_id` Array(String),
-    `abnormals_score` Array(Int32),
-    `abnormals_count` Array(UInt32),
-    `excellents_type` Array(UInt16),
-    `excellents_rule_id` Array(String),
-    `excellents_score` Array(Int32),
-    `excellents_count` Array(UInt32),
-    `qc_word_source` Array(UInt8),
-    `qc_word_word` Array(String),
-    `qc_word_count` Array(UInt32),
-    `qid` Array(Int64),
-    `mark` String,
-    `mark_judge` Int32,
-    `mark_score` Int32,
-    `mark_score_add` Int32,
-    `mark_ids` Array(String),
-    `last_mark_id` String,
-    `human_check` UInt8,
-    `tag_score_stats_id` Array(String),
-    `tag_score_stats_score` Array(Int32),
-    `tag_score_stats_count` Array(UInt32),
-    `tag_score_stats_md` Array(UInt8),
-    `tag_score_stats_mm` Array(UInt8),
-    `tag_score_add_stats_id` Array(String),
-    `tag_score_add_stats_score` Array(Int32),
-    `tag_score_add_stats_count` Array(UInt32),
-    `tag_score_add_stats_md` Array(UInt8),
-    `tag_score_add_stats_mm` Array(UInt8),
-    `rule_stats_id` Array(String),
-    `rule_stats_score` Array(Int32),
-    `rule_stats_count` Array(UInt32),
-    `rule_add_stats_id` Array(String),
-    `rule_add_stats_score` Array(Int32),
-    `rule_add_stats_count` Array(UInt32),
-    `xrule_stats_id` Array(String),
-    `xrule_stats_score` Array(Int32),
-    `xrule_stats_count` Array(UInt32),
-    `top_xrules_id` Array(String),
-    `top_xrules_score` Array(Int32),
-    `top_xrules_count` Array(UInt32),
-    `score` Int32,
-    `score_add` Int32,
-    `question_count` UInt32,
-    `answer_count` UInt32,
-    `first_answer_time` DateTime64(3),
-    `qa_time_sum` UInt32,
-    `qa_round_sum` UInt32,
-    `focus_goods_id` String,
-    `is_remind` UInt8,
-    `task_list_id` String,
-    `read_mark` Array(String),
-    `last_msg_id` String,
-    `consulte_transfor_v2` Int32,
-    `order_info_id` Array(String),
-    `order_info_status` Array(String),
-    `order_info_payment` Array(Float32),
-    `order_info_time` Array(UInt64),
-    `intel_score` Int32,
-    `remind_ntype` String,
-    `first_follow_up_time` DateTime64(3),
-    `is_follow_up_remind` UInt8,
-    `emotion_detect_mode` Int32,
-    `has_withdraw_robot_msg` UInt8,
-    `is_order_matched` UInt8,
-    `suspected_positive_emotion` UInt8,
-    `suspected_problem` UInt8,
-    `suspected_excellent` UInt8,
-    `has_after` UInt8,
-    `cnick_customize_rule` Array(String),
-    `update_time` DateTime('Asia/Shanghai'),
-    `wx_rule_stats_id` Array(String),
-    `wx_rule_stats_score` Array(Int32),
-    `wx_rule_stats_count` Array(UInt32),
-    `wx_rule_add_stats_id` Array(String),
-    `wx_rule_add_stats_score` Array(Int32),
-    `wx_rule_add_stats_count` Array(UInt32)
-)
-ENGINE = ReplicatedMergeTree(
-    '/clickhouse/{database}/tables/{layer}_{shard}/{table}',
-    '{replica}'
-)
-PARTITION BY toYYYYMMDD(begin_time)
-ORDER BY (platform, seller_nick, _id)
-SETTINGS storage_policy = 'rr', index_granularity = 8192
+    `superior_name` String,
+    `update_time` DateTime
+) ENGINE = ReplicatedReplacingMergeTree(
+    '/clickhouse/xqc_ods/tables/{layer}_{shard}/alert_local',
+    '{replica}',
+    update_time
+) PARTITION BY day PRIMARY KEY (level, warning_type)
+ORDER BY (level, warning_type, id) SETTINGS index_granularity = 8192,
+    storage_policy = 'rr';
+
+-- DROP TABLE xqc_ods.alert_all ON CLUSTER cluster_3s_2r NO DELAY;
+CREATE TABLE xqc_ods.alert_all ON CLUSTER cluster_3s_2r
+(
+    `id` String,
+    `platform` String,
+    `level` Int64,
+    `warning_type` String,
+    `dialog_id` String,
+    `message_id` String,
+    `time` String,
+    `day` Int64,
+    `is_finished` String,
+    `finish_time` String,
+    `shop_id` String,
+    `seller_nick` String,
+    `snick` String,
+    `cnick` String,
+    `buyer_one_id` String,
+    `open_uid` String,
+    `employee_name` String,
+    `superior_name` String,
+    `update_time` DateTime
+) ENGINE = Distributed(
+    'cluster_3s_2r',
+    'xqc_ods',
+    'alert_local',
+    xxHash64(level, warning_type, id)
+);
+
+-- DROP TABLE buffer.xqc_ods_alert_buffer ON CLUSTER cluster_3s_2r NO DELAY;
+CREATE TABLE buffer.xqc_ods_alert_buffer ON CLUSTER cluster_3s_2r
+AS xqc_ods.alert_all
+ENGINE = Buffer(
+    'xqc_ods',
+    'alert_all',
+    16,
+    5,
+    10,
+    81920,
+    409600,
+    16777216,
+    67108864
+);
+
+-- DROP TABLE buffer.xqc_ods_alert_buffer ON CLUSTER cluster_3s_2r NO DELAY;
+
+
+CREATE TABLE buffer.xqc_ods_alert_buffer ON CLUSTER cluster_3s_2r
+(
+    `id` String,
+    `platform` String,
+    `level` Int64,
+    `warning_type` String,
+    `dialog_id` String,
+    `message_id` String,
+    `time` String,
+    `day` Int64,
+    `is_finished` String,
+    `finish_time` String,
+    `shop_id` String,
+    `seller_nick` String,
+    `snick` String,
+    `cnick` String,
+    `buyer_one_id` String,
+    `open_uid` String,
+    `employee_name` String,
+    `superior_name` String,
+    `update_time` DateTime
+) ENGINE = Buffer(
+    'xqc_ods',
+    'alert_all',
+    16,
+    5,
+    10,
+    81920,
+    409600,
+    16777216,
+    67108864
+);
