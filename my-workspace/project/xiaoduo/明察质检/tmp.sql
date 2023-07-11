728eac652fc6f1c31536bb8907638763b39132d6
TomAndersen <1040994898@qq.com>
2023-07-11 20:01:09 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp.sql
@@ -1,109 +1,54 @@
--- DROP TABLE xqc_ods.alert_local ON CLUSTER cluster_3s_2r NO DELAY;
-CREATE TABLE xqc_ods.alert_local ON CLUSTER cluster_3s_2r
-(
-    `id` String,
-    `platform` String,
-    `level` Int64,
-    `warning_type` String,
-    `dialog_id` String,
-    `message_id` String,
-    `time` String,
-    `day` Int64,
-    `is_finished` String,
-    `finish_time` String,
-    `shop_id` String,
-    `seller_nick` String,
-    `snick` String,
-    `cnick` String,
-    `buyer_one_id` String,
-    `open_uid` String,
-    `employee_name` String,
-    `superior_name` String,
-    `update_time` DateTime
-) ENGINE = ReplicatedReplacingMergeTree(
-    '/clickhouse/xqc_ods/tables/{layer}_{shard}/alert_local',
-    '{replica}',
-    update_time
-) PARTITION BY day PRIMARY KEY (level, warning_type)
-ORDER BY (level, warning_type, id) SETTINGS index_granularity = 8192,
-    storage_policy = 'rr';
-
--- DROP TABLE xqc_ods.alert_all ON CLUSTER cluster_3s_2r NO DELAY;
-CREATE TABLE xqc_ods.alert_all ON CLUSTER cluster_3s_2r
-(
-    `id` String,
-    `platform` String,
-    `level` Int64,
-    `warning_type` String,
-    `dialog_id` String,
-    `message_id` String,
-    `time` String,
-    `day` Int64,
-    `is_finished` String,
-    `finish_time` String,
-    `shop_id` String,
-    `seller_nick` String,
-    `snick` String,
-    `cnick` String,
-    `buyer_one_id` String,
-    `open_uid` String,
-    `employee_name` String,
-    `superior_name` String,
-    `update_time` DateTime
-) ENGINE = Distributed(
-    'cluster_3s_2r',
-    'xqc_ods',
-    'alert_local',
-    xxHash64(level, warning_type, id)
-);
-
--- DROP TABLE buffer.xqc_ods_alert_buffer ON CLUSTER cluster_3s_2r NO DELAY;
-CREATE TABLE buffer.xqc_ods_alert_buffer ON CLUSTER cluster_3s_2r
-AS xqc_ods.alert_all
-ENGINE = Buffer(
-    'xqc_ods',
-    'alert_all',
-    16,
-    5,
-    10,
-    81920,
-    409600,
-    16777216,
-    67108864
-);
-
--- DROP TABLE buffer.xqc_ods_alert_buffer ON CLUSTER cluster_3s_2r NO DELAY;
-
-
-CREATE TABLE buffer.xqc_ods_alert_buffer ON CLUSTER cluster_3s_2r
-(
-    `id` String,
-    `platform` String,
-    `level` Int64,
-    `warning_type` String,
-    `dialog_id` String,
-    `message_id` String,
-    `time` String,
-    `day` Int64,
-    `is_finished` String,
-    `finish_time` String,
-    `shop_id` String,
-    `seller_nick` String,
-    `snick` String,
-    `cnick` String,
-    `buyer_one_id` String,
-    `open_uid` String,
-    `employee_name` String,
-    `superior_name` String,
-    `update_time` DateTime
-) ENGINE = Buffer(
-    'xqc_ods',
-    'alert_all',
-    16,
-    5,
-    10,
-    81920,
-    409600,
-    16777216,
-    67108864
-);
+SELECT
+    day, shop_id, snick, cnick,
+    arrayStringConcat(groupArray(content),'\n') AS contents
+FROM (
+    SELECT
+        day,
+        shop_id, snick, cnick, 
+        msg_time, msg,
+        toString(IF(act='send_msg', concat(snick, ' ', toString(msg_time), ':\n', msg), concat(cnick, ' ', toString(msg_time), ':\n', msg))) AS content
+    FROM ods.xdrs_logs_all
+    -- 筛选指定时间范围(强制限制近3天)
+    WHERE day BETWEEN toYYYYMMDD(toDate('{{end_date}}') - 3)
+        AND toYYYYMMDD(toDate('{{end_date}}'))
+    -- 筛选普通消息
+    AND act IN ['send_msg', 'recv_msg']
+    -- 筛选指定店铺
+    AND shop_id = '{{shop_id}}'
+    -- 筛选流失客户
+    AND cnick GLOBAL IN (
+        -- 筛选x天内, 发生过y次会话, 未下单, 买家信息
+        SELECT DISTINCT
+            concat('cntaobao', cnick) AS plat_cnick
+        FROM (
+            SELECT
+                platform,
+                real_buyer_nick,
+                cnick,
+                COUNT(DISTINCT _id) AS dialog_cnt
+            FROM dwd.xdqc_dialog_all
+                -- 筛选指定时间范围(强制限制近3天)
+            WHERE toYYYYMMDD(begin_time) BETWEEN toYYYYMMDD(toDate('{{end_date}}') -3)
+                AND toYYYYMMDD(toDate('{{end_date}}'))
+                -- 筛选会话轮次
+                AND qa_round_sum > toUInt32('{{dialog_rount}}')
+                -- 筛选指定平台
+                AND platform = '{{platform}}'
+                -- 筛选指定店铺-主账号
+                AND seller_nick = '{{seller_nick}}'
+                -- 筛选最近未下单买家, 即剔除已下单买家
+                AND length(order_info_status) = 0
+                -- 筛选备注信息
+                AND mark = '{{mark}}'
+            GROUP BY
+                platform, seller_nick, real_buyer_nick, cnick
+            HAVING
+                -- 筛选会话数量
+                dialog_cnt > toUInt32('{{dialog_cnt}}')
+        ) AS cnick_info
+        -- 限制买家数量
+        LIMIT 20
+    )
+    ORDER BY day, shop_id, snick, cnick, msg_time
+)
+GROUP BY day, shop_id, snick, cnick
