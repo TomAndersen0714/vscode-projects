82ade19dc54ba401f866e44ada54a45ac9d2461f
TomAndersen <1040994898@qq.com>
2023-07-19 00:39:11 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp1.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp1.sql
@@ -1,65 +1,50 @@
-CREATE TABLE xqc_ods.dialog_all ON CLUSTER cluster_3s_2r
-(
-    `id` String,
-    `platform` String,
-    `shop_id` String,
-    `seller_nick` String,
-    `snick` String,
-    `cnick` String,
-    `employee_name` String,
-    `superior_name` String,
-    `time` String,
-    `hour` Int64,
-    `day` Int64
-) ENGINE = Distributed(
-    'cluster_3s_2r',
-    'xqc_ods',
-    'dialog_local',
-    rand()
-)
+        // exclude boundary condition
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return res;
+        }
 
-CREATE TABLE xqc_ods.dialog_local ON CLUSTER cluster_3s_2r
-(
-    `id` String,
-    `platform` String,
-    `shop_id` String,
-    `seller_nick` String,
-    `snick` String,
-    `cnick` String,
-    `employee_name` String,
-    `superior_name` String,
-    `time` String,
-    `hour` Int64,
-    `day` Int64
-) ENGINE = ReplicatedMergeTree(
-    '/clickhouse/xqc_ods/tables/{layer}_{shard}/dialog_local',
-    '{replica}'
-) PARTITION BY (day, platform)
-ORDER BY snick SETTINGS index_granularity = 8192,
-    storage_policy = 'rr'
+        // handle
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int target = -nums[i];
+            int left = i + 1, right = nums.length - 1;
 
+            while (left < right) {
+                if (nums[left] + nums[right] < target) {
+                    left += 1;
+                }
+                else if (nums[left] + nums[right] > target) {
+                    right -= 1;
+                }
+                else {
+                    // List<Integer> tuple = new ArrayList<>();
+                    // tuple.add(nums[i]);
+                    // tuple.add(nums[left]);
+                    // tuple.add(nums[right]);
+                    // res.add(tuple);
 
-CREATE TABLE buffer.xqc_ods_dialog_buffer ON CLUSTER cluster_3s_2r
-(
-    `id` String,
-    `platform` String,
-    `shop_id` String,
-    `seller_nick` String,
-    `snick` String,
-    `cnick` String,
-    `employee_name` String,
-    `superior_name` String,
-    `time` String,
-    `hour` Int64,
-    `day` Int64
-) ENGINE = Buffer(
-    'xqc_ods',
-    'dialog_all',
-    16,
-    5,
-    10,
-    81920,
-    409600,
-    16777216,
-    67108864
-)
+                    res.add(Arrays.asList(nums[i], nums[left],nums[right]));
+
+                    // there might be multiple tuples
+                    left += 1;
+                    right -= 1;
+
+                    // skip duplicate elements in second and third position to avoid duplicate tuple
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left += 1;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right -= 1;
+                    }
+                }
+            }
+
+            // skip duplicate elements in first position to avoid duplicate tuple
+            while (i + 1 < nums.length - 2 && nums[i] == nums[i + 1]) {
+                i += 1;
+            }
+        }
+
+        // return
+        return res;
