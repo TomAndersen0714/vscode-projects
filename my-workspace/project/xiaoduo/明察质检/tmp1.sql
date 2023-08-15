48d3b0b7d8fb73fea2fff3b022d8ce8c5fa3fbd1
TomAndersen <1040994898@qq.com>
2023-08-15 19:03:10 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp1.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp1.sql
@@ -1,177 +1,56 @@
 SELECT
-    day,
-    company_id, shop_id, platform, seller_nick, snick,
-    tag_dim.qc_norm_id,
-    tag_dim.qc_norm_name,
-    tag_dim.qc_norm_group_id,
-    tag_dim.qc_norm_group_name,
-    tag_dim.qc_norm_group_full_name,
-    tag_type, tag_id, tag_dim.tag_name,
-    tag_cnt_sum, tag_score_sum, tag_dialog_cnt, tag_manual_dialog_cnt
+    day, shop_id, snick, cnick,
+    arrayStringConcat(groupArray(content),'\n') AS contents
 FROM (
-    SELECT
-        day,
-        company_id, shop_id, platform, seller_nick, snick,
-        tag_type, tag_id,
-        tag_cnt_sum, tag_score_sum, tag_dialog_cnt, tag_manual_dialog_cnt
+    SELECT * 
     FROM (
         SELECT
             day,
-            platform, seller_nick, snick,
-            tag_type, tag_id,
-            SUM(tag_cnt) AS tag_cnt_sum,
-            -- 同一个ID分数可能发生变化, 以实际打标为准
-            SUM(tag_score*tag_cnt) AS tag_score_sum,
-            uniqExact(dialog_id) AS tag_dialog_cnt,
-            uniqExactIf(dialog_id, manual_checked>0) AS tag_manual_dialog_cnt
-        FROM (
-            -- 新版本AI质检项-非情绪扣分项
+            shop_id, snick, cnick, 
+            msg_time, msg,
+            toString(IF(act='send_msg', concat(snick, ' ', toString(msg_time), ':\n', msg), concat(cnick, ' ', toString(msg_time), ':\n', msg))) AS content
+        FROM ods.xdrs_logs_all
+        -- 筛选指定时间范围(强制限制近7天)
+        PREWHERE day BETWEEN toYYYYMMDD(toDate('{{end_date}}') - 3)
+            AND toYYYYMMDD(toDate('{{end_date}}'))
+        -- 筛选普通消息
+        AND act IN ['send_msg', 'recv_msg']
+        -- 筛选指定店铺
+        AND shop_id GLOBAL IN (
+            SELECT shop_id
+            FROM xqc_dim.shop_latest_all
+            WHERE company_id = '{{company_id}}'
+            AND platform = '{{platform}}'
+        )
+        -- 筛选买家和客服账号
+        AND (snick, cnick) GLOBAL IN (
+            -- 筛选包含指定标签的
             SELECT
-                toYYYYMMDD(begin_time) AS day,
-                platform,
-                seller_nick,
-                snick,
-                'ai_abnormal' AS tag_type,
-                _id AS dialog_id,
-                notEmpty(last_mark_id) AS manual_checked,
-                abnormals_rule_id AS tag_ids,
-                abnormals_count AS tag_cnts,
-                abnormals_score AS tag_scores
-            FROM dwd.xdqc_dialog_all
-            WHERE toYYYYMMDD(begin_time) BETWEEN 20230801 AND 20230803
-            AND platform = 'jd'
-            AND seller_nick IN ['顾家家居京东自营旗舰店', '顾家家居官方旗舰店']
-            -- 过滤新版本AI质检-扣分项
-            AND arrayExists((x)->x!='',abnormals_rule_id)
-
-            -- 新版本AI质检项-非情绪加分项
-            UNION ALL
-            SELECT
-                toYYYYMMDD(begin_time) AS day,
-                platform,
-                seller_nick,
-                snick,
-                'ai_excellent' AS tag_type,
-                _id AS dialog_id,
-                notEmpty(last_mark_id) AS manual_checked,
-                excellents_rule_id AS tag_ids,
-                excellents_count AS tag_cnts,
-                excellents_score AS tag_scores
-            FROM dwd.xdqc_dialog_all
-            WHERE toYYYYMMDD(begin_time) BETWEEN 20230801 AND 20230803
-            AND platform = 'jd'
-            AND seller_nick IN ['顾家家居京东自营旗舰店', '顾家家居官方旗舰店']
-            -- 过滤新版本AI质检-加分项
-            AND arrayExists((x)->x!='',excellents_rule_id)
-
-            -- 新版本AI质检项-买家情绪项
-            UNION ALL
-            SELECT
-                toYYYYMMDD(begin_time) AS day,
-                platform,
-                seller_nick,
-                snick,
-                'ai_c_emotion' AS tag_type,
-                _id AS dialog_id,
-                notEmpty(last_mark_id) AS manual_checked,
-                c_emotion_rule_id AS tag_ids,
-                c_emotion_count AS tag_cnts,
-                c_emotion_score AS tag_scores
-            FROM dwd.xdqc_dialog_all
-            WHERE toYYYYMMDD(begin_time) BETWEEN 20230801 AND 20230803
-            AND platform = 'jd'
-            AND seller_nick IN ['顾家家居京东自营旗舰店', '顾家家居官方旗舰店']
-            -- 过滤新版本AI质检-买家情绪项
-            AND arrayExists((x)->x!='',c_emotion_rule_id)
-
-            -- 新版本AI质检项-客服情绪项
-            UNION ALL
-            SELECT
-                toYYYYMMDD(begin_time) AS day,
-                platform,
-                seller_nick,
-                snick,
-                'ai_s_emotion' AS tag_type,
-                _id AS dialog_id,
-                notEmpty(last_mark_id) AS manual_checked,
-                s_emotion_rule_id AS tag_ids,
-                s_emotion_count AS tag_cnts,
-                s_emotion_score AS tag_scores
-            FROM dwd.xdqc_dialog_all
-            WHERE toYYYYMMDD(begin_time) BETWEEN 20230801 AND 20230803
-            AND platform = 'jd'
-            AND seller_nick IN ['顾家家居京东自营旗舰店', '顾家家居官方旗舰店']
-            -- 过滤新版本AI质检-客服情绪项
-            AND arrayExists((x)->x!='',s_emotion_rule_id)
-        ) AS ods_ai_tag
-        ARRAY JOIN
-            tag_ids AS tag_id,
-            tag_cnts AS tag_cnt,
-            tag_scores AS tag_score
-        -- 排除空数据
-        WHERE tag_id!='' AND tag_cnt!=0
-        GROUP BY
-            day, platform, seller_nick, snick, tag_type, tag_id
-    ) AS tag_stat
-    GLOBAL LEFT JOIN (
-        SELECT
-            company_id, shop_id, platform, seller_nick, snick
-        FROM xqc_dim.snick_full_info_all
-        WHERE day = 20230813
-    ) AS snick_dim
-    USING(platform, seller_nick, snick)
-) AS ods_ai_tag
-GLOBAL LEFT JOIN (
-    SELECT
-        company_id,
-        platform,
-        tag_info.tag_id,
-        tag_info.tag_name,
-        tag_info.qc_norm_id,
-        tag_info.qc_norm_group_id,
-        tag_group_info.qc_norm_name,
-        tag_group_info.qc_norm_group_name,
-        tag_group_info.qc_norm_group_full_name
-    FROM (
-        SELECT
-            _id AS tag_id,
-            name AS tag_name,
-            qc_norm_id,
-            qc_norm_group_id
-        FROM xqc_dim.qc_rule_all
-        WHERE day = 20230813
-    ) AS tag_info
-    GLOBAL INNER JOIN (
-        SELECT
-            company_id,
-            platform,
-            qc_norm_id,
-            qc_norm_name,
-            qc_norm_group_id,
-            qc_norm_group_name,
-            qc_norm_group_full_name
-        FROM (
-            SELECT
-                qc_norm_id,
-                _id AS qc_norm_group_id,
-                name AS qc_norm_group_name,
-                full_name AS qc_norm_group_full_name
-            FROM xqc_dim.qc_norm_group_full_all
-            WHERE day = 20230813
-        ) AS qc_norm_group_info
-        GLOBAL INNER JOIN (
-            SELECT
-                company_id,
-                platform,
-                _id AS qc_norm_id,
-                name AS qc_norm_name
-            FROM ods.xinghuan_qc_norm_all
-            WHERE day = 20230813
-            AND qc_norm_id != ''
-        ) AS qc_norm_info
-        USING(qc_norm_id)
-        WHERE qc_norm_id != '' AND qc_norm_group_id != ''
-    ) AS tag_group_info
-    USING(qc_norm_id, qc_norm_group_id)
-) AS tag_dim
-USING(company_id, tag_id)
+                concat('cntaobao', snick) AS plat_snick,
+                concat('cntaobao', cnick) AS plat_cnick
+            FROM (
+                SELECT
+                    snick,
+                    cnick
+                FROM dwd.xdqc_dialog_all
+                    -- 筛选指定时间范围(强制限制近7天)
+                WHERE toYYYYMMDD(begin_time) BETWEEN toYYYYMMDD(toDate('{{end_date}}') -3)
+                    AND toYYYYMMDD(toDate('{{end_date}}'))
+                    -- 筛选"顾客提及投诉和举报"打标次数>0的会话
+                    AND abnormals_count[22] > 0
+                    -- 筛选指定店铺主账号
+                    AND seller_nick GLOBAL IN (
+                        SELECT seller_nick
+                        FROM xqc_dim.shop_latest_all
+                        WHERE company_id = '{{company_id}}'
+                        AND platform = '{{platform}}'
+                    )
+                LIMIT 100
+            ) AS cnick_info
+            -- 限制会话数量
+            LIMIT 100
+        )
+    )
+    ORDER BY day, shop_id, snick, cnick, msg_time
+)
+GROUP BY day, shop_id, snick, cnick
