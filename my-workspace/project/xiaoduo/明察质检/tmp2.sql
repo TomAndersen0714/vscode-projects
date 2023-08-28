f92868ed04c6bddb2e36f8ad22d6178dc2cbe201
TomAndersen <1040994898@qq.com>
2023-08-28 09:39:23 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp2.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp2.sql
@@ -1,38 +1,4 @@
-DELETE FROM polaris.shop_overview_dd
-WHERE day = 20230823
-AND platform = 'ks'
-AND shop_id IN (
-    SELECT DISTINCT shop_id
-    FROM (
-        SELECT day, shop_id, count(1) AS cnt
-        FROM polaris.shop_overview_dd
-        WHERE day = 20230823
-        AND shop_id != ''
-        GROUP BY day, shop_id
-        HAVING count(1) > 1
-    ) AS tmp
-)
-
-
-DELETE FROM polaris.shop_overview_dd
-WHERE day = 20230820
-AND platform = 'ks'
-AND shop_id IN (
-    SELECT DISTINCT shop_id
-    FROM (
-        SELECT day, shop_id, count(1) AS cnt
-        FROM polaris.shop_overview_dd
-        WHERE day = 20230820
-        AND shop_id != ''
-        GROUP BY day, shop_id
-        HAVING count(1) > 1
-    ) AS tmp
-)
-
-INSERT INTO tmp.shop_overview_dd_0825
-SELECT * FROM polaris.shop_overview_dd
-WHERE day = 20230820
-
-INSERT INTO tmp.shop_overview_dd_0825
-SELECT * FROM polaris.shop_overview_dd
-WHERE day = 20230823
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
