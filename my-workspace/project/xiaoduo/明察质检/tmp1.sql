a1e8fff6d94c4efbaf9371f989cb8b6a0ef89336
TomAndersen <1040994898@qq.com>
2023-08-31 18:49:41 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp1.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp1.sql
@@ -1,35 +1,11 @@
-select count(1) as count
-from (
-        select distinct buyer_nick
-        from (
-                select distinct final_table_0.buyer_nick
-                from (
-                        select distinct buyer_nick
-                        from (
-                                select buyer_nick,
-                                    avg(payment) as avg_payment_paid
-                                from ods.order_event_all
-                                where shop_id = '5ec76879edbe97000f8d850c'
-                                    and status = 'paid'
-                                group by buyer_nick
-                                having avg_payment_paid between 100 and 100
-                            )
-                    ) as final_table_0
-            ) as table_groupresult
-            join (
-                SELECT DISTINCT buyer_nick
-                FROM (
-                        SELECT DISTINCT buyer_nick
-                        FROM ods.order_event_all
-                        WHERE `day` > toYYYYMMDD(subtractDays(now(), 60))
-                            AND shop_id = '5ec76879edbe97000f8d850c'
-                            AND status = 'created'
-                        UNION ALL
-                        SELECT DISTINCT buyer_nick
-                        FROM ods.chat_event_all
-                        WHERE `day` > toYYYYMMDD(subtractDays(now(), 60))
-                            AND shop_id = '5ec76879edbe97000f8d850c'
-                            AND act = 'consult'
-                    )
-            ) as table_reachable on table_reachable.buyer_nick = table_groupresult.buyer_nick
-    ) -- trace:1028b7ca5083d232fed82ae013239fda
+SELECT DISTINCT
+    cnick
+FROM (
+    SELECT
+        replaceOne(cnick,'cntaobao','') AS cnick
+    FROM ods.xdrs_logs_all
+    WHERE day = 20230826
+    AND shop_id = '60b72d421edc070017428380'
+    AND act IN ['send_msg', 'recv_msg']
+    AND plat_goods_id = '718154483681'
+)
--- my-workspace/project/xiaoduo/明察质检/tmp4.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp4.sql
@@ -1,30 +1,51 @@
-SELECT *
-FROM (
+
     SELECT
-        day,
-        platform,
-        cnick,
-        real_buyer_nick
+        groupBitmapOr(cnick_id_bitmap)
     FROM (
-        SELECT DISTINCT
+        SELECT
             day,
             platform,
-            replaceOne(cnick,'cntaobao','') AS cnick,
-            length(cnick) AS cnick_len,
-            real_buyer_nick
-        FROM ods.xdrs_logs_all
-        PREWHERE day = 20230826
-        AND shop_id GLOBAL IN (
-            SELECT shop_id
-            FROM xqc_dim.shop_latest_all
-            WHERE company_id GLOBAL IN (
-                SELECT _id
-                FROM xqc_dim.company_latest_all
-                WHERE has(white_list, 'VOC')
-            )
-            AND platform IN ['tb']
+            shop_id,
+            snick,
+            question_id,
+            dialog_qa_stage,
+            dialog_goods_id,
+            recent_order_id,
+            recent_order_status,
+            recent_order_status_timestamp,
+            groupBitmapState(cnick_id) AS cnick_id_bitmap,
+            bitmapCardinality(cnick_id_bitmap) AS dialog_sum
+        FROM (
+            SELECT
+                day,
+                platform,
+                shop_id,
+                snick,
+                cnick_id,
+                question_b_qid AS question_id,
+                CASE
+                    WHEN dialog_qa_sum=0 THEN 0
+                    WHEN dialog_qa_sum>0 AND dialog_qa_sum<=3 THEN 1
+                    WHEN dialog_qa_sum>3 AND dialog_qa_sum<=10 THEN 2
+                    ELSE 3
+                END AS dialog_qa_stage,
+                plat_goods_id AS dialog_goods_id,
+                recent_order_id,
+                recent_order_status,
+                recent_order_status_timestamp
+            FROM dwd.voc_chat_log_detail_all
+            WHERE day = 20230826
+            AND shop_id = '60b72d421edc070017428380'
+            AND plat_goods_id = '718154483681'
         )
-        AND shop_id = '60b72d421edc070017428380'
-        AND cnick = 'one_id_397347653'
-    ) AS today_cnick_list
-)
+        GROUP BY day,
+            platform,
+            shop_id,
+            snick,
+            question_id,
+            dialog_qa_stage,
+            dialog_goods_id,
+            recent_order_id,
+            recent_order_status,
+            recent_order_status_timestamp
+    ) AS question_stat_info
