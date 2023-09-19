3071436d7e1ab7eec9461d41201e4599c4844969
TomAndersen <1040994898@qq.com>
2023-09-19 11:56:42 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp.sql
@@ -1,35 +1,11 @@
-SELECT
-    province, city, gdp, gdp/province_gdp AS gdp_pct,
-    row_number() over(partition by province order by gdp desc) AS gdp_rank
-FROM city_gdp_table
-JOIN (
-    SELECT province, SUM(gdp) AS province_gdp
-    FROM city_gdp
-    GROUP BY province
-) AS province_gdp_table
-USING(province)
-
-
-1. 先求各部门平均分, 然后筛选平均分大于80的部门
-SELECT
-    dept_id,
-    AVG(score) AS avg_score
-FROM Emp
-GROUP BY dept_id
-HAVING avg_score>80
-
-2. 关联部门名称
-SELECT
-    dept_id,
-    dept_name,
-    avg_score
-FROM (
-    SELECT
-        dept_id,
-        AVG(score) AS avg_score
-    FROM Emp
-    GROUP BY dept_id
-    HAVING avg_score>80
-) AS Dept_score_tmp
-JOIN Dept
-USING(dept_id)
+select
+    hostName(),
+    *
+from remote('{{host}}', 'system.query_log')
+where toYYYYMMDD(event_date) BETWEEN toYYYYMMDD(toDateTime('{{datetime.start}}')) AND toYYYYMMDD(toDateTime('{{datetime.end}}'))
+    and event_time BETWEEN toDateTime('{{datetime.start}}') AND toDateTime('{{datetime.end}}')
+    and memory_usage >= {{memory_mb_threshold}}*1024*1024
+    and type in [{{type}}]
+    and query like '%{{query}}%'
+order by ({{desc_order_key}}) desc
+limit {{limit}}
--- my-workspace/project/xiaoduo/明察质检/tmp1.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp1.sql
@@ -1,14 +1,6 @@
-SELECT city.province,
-    city.name AS city_name,
-    city.gdp / province.total_gdp AS gdp_ratio,
-    ROW_NUMBER() OVER (
-        PARTITION BY city.province
-        ORDER BY city.gdp DESC
-    ) AS ranking_in_province
-FROM city
-    JOIN (
-        SELECT province,
-            SUM(gdp) AS total_gdp
-        FROM city
-        GROUP BY province
-    ) AS province ON city.province = province.province;
+10.22.113.168:19000
+10.22.113.169:19000
+10.22.113.173:19000
+10.22.113.174:29000
+10.22.113.175:29000
+10.22.133.216:29000
