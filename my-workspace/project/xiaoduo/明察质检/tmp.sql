15c567832b6ee6e5aa20157aed99b0bcee9d7103
TomAndersen <1040994898@qq.com>
2023-09-20 17:55:52 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp.sql
@@ -1,58 +1,14 @@
-SELECT
-    day, shop_id, snick, cnick,
-    arrayStringConcat(groupArray(content),'\n') AS contents
-FROM (
-    SELECT
-        day,
-        shop_id, snick, cnick, 
-        msg_time, msg,
-        toString(IF(act='send_msg', concat(snick, ' ', toString(msg_time), ':\n', msg), concat(cnick, ' ', toString(msg_time), ':\n', msg))) AS content
-    FROM ods.xdrs_logs_all
-    -- 筛选指定时间范围(强制限制近3天)
-    WHERE day BETWEEN toYYYYMMDD(toDate('{{end_date}}') - 3)
-        AND toYYYYMMDD(toDate('{{end_date}}'))
-    -- 筛选普通消息
-    AND act IN ['send_msg', 'recv_msg']
-    
-    --筛选买家消息里面包含小二的话术
-    WHERE recv_msg LIKE '%小二%'
-
-    -- 筛选指定店铺
-    AND shop_id = '{{shop_id}}'
-    -- 筛选流失客户
-    AND cnick GLOBAL IN (
-        -- 筛选x天内, 发生过y次会话, 未下单, 买家信息
-        SELECT DISTINCT
-            concat('cntaobao', cnick) AS plat_cnick
-        FROM (
-            SELECT
-                platform,
-                real_buyer_nick,
-                cnick,
-                COUNT(DISTINCT _id) AS dialog_cnt
-            FROM dwd.xdqc_dialog_all
-                -- 筛选指定时间范围(强制限制近3天)
-            WHERE toYYYYMMDD(begin_time) BETWEEN toYYYYMMDD(toDate('{{end_date}}') -3)
-                AND toYYYYMMDD(toDate('{{end_date}}'))
-                -- 筛选会话轮次
-                AND qa_round_sum > toUInt32('{{会话轮次}}')
-                -- 筛选指定平台
-                AND platform = '{{platform}}'
-                -- 筛选指定店铺-主账号
-                AND seller_nick = '{{seller_nick}}'
-                -- 筛选最近未下单买家, 即剔除已下单买家
-                AND length(order_info_status) = 0
-            GROUP BY
-                platform, seller_nick, real_buyer_nick, cnick
-            HAVING
-                -- 筛选会话数量
-                dialog_cnt > toUInt32('{{dialog_cnt}}')
-        ) AS cnick_info
-        -- 限制买家数量
-        LIMIT 20
-    )
-    -- 在这里添加限制snick包含"服务助手"的条件
-    -- AND snick LIKE '%服务助手%'
-    ORDER BY day, shop_id, snick, cnick, msg_time
-)
-GROUP BY day, shop_id, snick, cnick
+dags/clean_partition_daily.py
+dags/data_sync_base_customer_health.py
+dags/data_sync_base_customer_health_v2.py
+dags/dipper_all_imp_to_ch_daily.py
+dags/dipper_ask_order_conversion_stat_sync_daily.py
+dags/dipper_customer_life_cycle_daily.py
+dags/dipper_dy_to_tb_kudu_daily.py
+dags/dipper_ks_customer_life_cycle_daily.py
+dags/DI_sql_health_v3.py
+dags/dy_daily_reception_stat.py
+dags/jd_dipper_sync_to_kudu_daily.py
+dags/ks_xdrs_log_mongo2impala_day.py
+dags/sync_dipper_impala2ch.py
+dags/tb_dipper_pdd_daily.py
