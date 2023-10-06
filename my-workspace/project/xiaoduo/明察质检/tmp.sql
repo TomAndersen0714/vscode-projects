cf1073968045cbb79d3df9018e1dd21be55b9185
TomAndersen <1040994898@qq.com>
2023-10-06 21:47:49 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp.sql
@@ -1,58 +1,49 @@
+create table `score`(
+`id` int(11),
+`class_no` varchar(11) default null,
+`course_name` varchar(11) default null,
+`student_no` varchar(11) default null,
+`student_score` int(11) default null,
+primary key(`id`)
+);
+
+insert into `score`(`class_no`, `course_name`, `student_no`, `student_score`) values('1', 'course_a', '1', 80);
+insert into `score`(`class_no`, `course_name`, `student_no`, `student_score`) values('1', 'course_b', '1', 80);
+insert into `score`(`class_no`, `course_name`, `student_no`, `student_score`) values('1', 'course_c', '1', 80);
+insert into `score`(`class_no`, `course_name`, `student_no`, `student_score`) values('1', 'course_b', '2', 80);
+insert into `score`(`class_no`, `course_name`, `student_no`, `student_score`) values('1', 'course_c', '2', 90);
+insert into `score`(`class_no`, `course_name`, `student_no`, `student_score`) values('1', 'course_b', '3', 80);
+insert into `score`(`class_no`, `course_name`, `student_no`, `student_score`) values('1', 'course_c', '3', 90);
+
 SELECT
-    day, shop_id, snick, cnick,
-    arrayStringConcat(groupArray(content),'\n') AS contents
+    class_no,
+    student_no,
+    score_sum
 FROM (
     SELECT
-        day,
-        shop_id, snick, cnick, 
-        msg_time, msg,
-        toString(IF(act='send_msg', concat(snick, ' ', toString(msg_time), ':\n', msg), concat(cnick, ' ', toString(msg_time), ':\n', msg))) AS content
-    FROM ods.xdrs_logs_all
-    -- 筛选指定时间范围(强制限制近3天)
-    WHERE day BETWEEN toYYYYMMDD(toDate('{{end_date}}') - 3)
-        AND toYYYYMMDD(toDate('{{end_date}}'))
-    -- 筛选普通消息
-    AND act IN ['send_msg', 'recv_msg']
+        class_no,
+        student_no,
+        score_sum,
+        dense_rank() OVER(ORDER BY score_sum DESC) AS d_rank
+    FROM (
+        SELECT
+            class_no,
+            student_no,
+            SUM(student_score) AS score_sum
+        FROM `score`
+        WHERE course_name = 'course_b' OR course_name = 'course_c'
+        GROUP BY class_no, student_no
+    ) AS tmp
+) AS rank_table
+WHERE d_rank = 2;
 
-    -- 筛选指定店铺
-    AND shop_id = '{{shop_id}}'
-    -- 筛选流失客户
-    AND cnick GLOBAL IN (
-        -- 筛选x天内, 发生过y次会话, 未下单, 买家信息
-        SELECT DISTINCT
-            concat('cntaobao', cnick) AS plat_cnick
-        FROM (
-            SELECT
-                platform,
-                real_buyer_nick,
-                cnick,
-                COUNT(DISTINCT _id) AS dialog_cnt
-            FROM dwd.xdqc_dialog_all
-                -- 筛选指定时间范围(强制限制近3天)
-            WHERE toYYYYMMDD(begin_time) BETWEEN toYYYYMMDD(toDate('{{end_date}}') -3)
-                AND toYYYYMMDD(toDate('{{end_date}}'))
-                -- 筛选会话轮次
-                AND qa_round_sum > toUInt32('{{会话轮次}}')
-                -- 筛选指定平台
-                AND platform = '{{platform}}'
-                -- 筛选指定店铺-主账号
-                AND seller_nick = '{{seller_nick}}'
-                -- 筛选最近未下单买家, 即剔除已下单买家
-                AND length(order_info_status) = 0
-            GROUP BY
-                platform, seller_nick, real_buyer_nick, cnick
-            HAVING
-                -- 筛选会话数量
-                dialog_cnt > toUInt32('{{dialog_cnt}}')
-        ) AS cnick_info
-        -- 限制买家数量
-        LIMIT 2000
-    )
-    -- 在这里添加限制snick包含"服务助手"的条件
-    -- AND snick LIKE '%服务助手%'
-    ORDER BY day, shop_id, snick, cnick, msg_time
-)
-GROUP BY day, shop_id, snick, cnick
---筛选买家消息里面包含小二的话术
-HAVING contents LIKE '%维权小二%' 
-    
+
+SELECT
+    class_no,
+    student_no,
+    SUM(student_score) AS score_sum
+FROM `score`
+WHERE course_name = 'course_b' OR course_name = 'course_c'
+GROUP BY class_no, student_no
+ORDER BY score_sum DESC
+LIMIT 1 OFFSET 1;
--- my-workspace/project/xiaoduo/明察质检/tmp1.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp1.sql
@@ -1,31 +1,36 @@
-表t1的数据
-id
-1
-2
-3
-表t2的数据
-id
-1
-2
-2
+CREATE TABLE `student` (
+    `id` int(11), //学号
+    `name` varchar(11) DEFAULT NULL, //学生姓名
+    `age` int(11) DEFAULT NULL, //年龄
+    `sex` varchar(11) DEFAULT NULL, //性别
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `score_relation` (
+    `id` int(11), //主键id
+    `course_no` int(11) DEFAULT NULL, //课程
+    `student_no` int(11) DEFAULT NULL,//学号
+    `score` int(11) DEFAULT NULL, //成绩
+    PRIMARY KEY (`id`)
+);
+
+查询至少有一门课与学号为"7"同学所学课程相同的同学, 查询这些学生的学号与姓名，去重后，按照学号升序排列。
 
 
--- 题目1 执行结果是什么样的
- select t1.id, t2.id
-from t1 left join t2 
-on t1.id = t2.id and t2.id <> 2;
+SELECT DISTINCT
+    `id`, `name`
+FROM `student`
+WHERE `id` IN (
+    SELECT
+        student_no
+    FROM `score_relation`
+    WHERE course_no IN (
+        SELECT DISTINCT
+            course_no
+        FROM `score_relation`
+        WHERE student_no = 7
+    ) AS course_no_table
+) AS student_no_table
+ORDER BY `id` ASC
 
-t1.id, t2.id:
-1, 1
-2, null
-3, null
 
-
--- 题目2 执行结果是什么样的
-select t1.id, t2.id
-from t1 left join t2 
-on t1.id = t2.id
-where t2.id <> 2；
-
-t1.id, t2.id:
-1, 1
