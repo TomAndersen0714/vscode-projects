dea4db620d92d091e9f9b4115ebc1baf884990bb
TomAndersen <1040994898@qq.com>
2023-10-19 19:17:08 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp.sql
@@ -1,59 +1,51 @@
--- 新实时告警-店铺告警-实时告警项
-WITH (
-    -- 告警总量
-    SELECT
-        COUNT(1)
-    FROM xqc_ods.alert_all FINAL
-    PREWHERE day BETWEEN toYYYYMMDD(toDate('{{ day.start }}')) 
-    AND toYYYYMMDD(toDate('{{ day.end }}'))
-    AND seller_nick = '华硕京东自营官方旗舰店'
-    -- 下拉框-告警项
-    AND (
-        '{{ warning_types }}' = ''
-        OR
-        warning_type IN splitByChar(',','{{ warning_types }}')
-    )
-) AS all_alert_sum
-SELECT
-    `level`,
-    warning_type as `告警项`,
-    sum(1) AS level_type_alert_cnt,
-    level_type_alert_cnt AS `告警总量`,
-    COUNT(DISTINCT dialog_id) AS `告警会话量`,
-    CONCAT(
-        toString(
-            if(
-                all_alert_sum != 0,
-                round(level_type_alert_cnt / all_alert_sum * 100, 2),
-                0.00
-            )
-        ),
-        '%'
-    ) AS `告警占比`,
-    sum(is_finished = 'False') AS not_finished_level_type_alert_cnt,
-    not_finished_level_type_alert_cnt AS `未处理量`,
-    CONCAT(
-        toString(
-            if(
-                level_type_alert_cnt != 0,
-                round((level_type_alert_cnt - not_finished_level_type_alert_cnt) / level_type_alert_cnt * 100,2),
-                0.00
-            )
-        ),
-        '%'
-    ) AS `告警完结率`
+SELECT task_id,
+    shop_id,
+(paid_payment + (payment / 100)) as paid_payment
 FROM (
-    SELECT *
-    FROM xqc_ods.alert_all FINAL
-    PREWHERE day BETWEEN toYYYYMMDD(toDate('{{ day.start }}')) 
-    AND toYYYYMMDD(toDate('{{ day.end }}'))
-    AND seller_nick = '华硕京东自营官方旗舰店'
-    -- 下拉框-告警项
-    AND (
-        '{{ warning_types }}' = ''
-        OR
-        warning_type IN splitByChar(',','{{ warning_types }}')
+        SELECT *
+        FROM app_fishpond.fishpond_task_stat
+    ) AS aa
+    JOIN (
+        SELECT task_id,
+            shop_id,
+            sum(payment) as payment
+        FROM (
+                SELECT t.task_id AS task_id,
+                    o.shop_id,
+                    o.order_id,
+                    o.payment AS payment
+                FROM (
+                        SELECT shop_id,
+                            order_id,
+                            max(payment) AS payment
+                        FROM ods.order_event_all
+                        WHERE `day` BETWEEN { day_15 } AND { day }
+                            AND order_id IN (
+                                SELECT DISTINCT order_id
+                                FROM ods.fishpond_conversion_all
+                                WHERE `day` = { day_15 }
+                                    AND status = 'paid'
+                                    AND payment = 0
+                            )
+                        GROUP BY shop_id,
+                            order_id
+                    ) AS o
+                    JOIN (
+                        SELECT DISTINCT shop_id,
+                            order_id,
+                            task_id
+                        FROM ods.fishpond_conversion_all
+                        WHERE `day` = { day_15 }
+                            AND status = 'paid'
+                            AND payment = 0
+                    ) AS t USING(
+                        shop_id,
+                        order_id
+                    )
+            )
+        GROUP BY shop_id,
+            task_id
+    ) AS a USING(
+        shop_id,
+        task_id
     )
-) AS alert_info
-GROUP BY `level`, warning_type
-ORDER BY `level` DESC, warning_type ASC
