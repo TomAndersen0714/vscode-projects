a1520f828bc1246e17c6aadb31c2a5a98c9673aa
TomAndersen <1040994898@qq.com>
2023-10-27 18:28:33 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp2.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp2.sql
@@ -1,4 +1,16 @@
--- ALTER TABLE app_fishpond.fishpond_task_stat ON CLUSTER cluster_3s_2r
--- DROP COLUMN IF EXISTS `platform`;
-ALTER TABLE app_fishpond.fishpond_task_stat ON CLUSTER cluster_3s_2r
-ADD COLUMN IF NOT EXISTS `platform` String AFTER `shop_id`;
+DROP TABLE app_fishpond.fishpond_task_stat ON CLUSTER cluster_3s_2r;
+
+CREATE TABLE app_fishpond.fishpond_task_stat ON CLUSTER cluster_3s_2r
+(
+    `task_id` String,
+    `shop_id` String,
+    `platform` String,
+    `send_uv` Int32,
+    `reply_uv` Int32,
+    `created_uv` Int32,
+    `created_pv` Int32,
+    `created_payment` Float64,
+    `paid_uv` Int32,
+    `paid_pv` Int32,
+    `paid_payment` Float64
+) ENGINE = MySQL('10.20.2.130:3306','app_fishpond','fishpond_task_stat','root','mypass');
