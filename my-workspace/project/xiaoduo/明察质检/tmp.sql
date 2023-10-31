e5946727ba7d31a2651e444aafb05f4a17c63918
TomAndersen <1040994898@qq.com>
2023-10-31 18:05:36 +0800

    update

--- my-workspace/project/xiaoduo/明察质检/tmp.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp.sql
@@ -1,14 +1,9 @@
--- ALTER TABLE ods.fishpond_conversion_local ON CLUSTER cluster_3s_2r 
+-- ALTER TABLE dim.fishpond_task_local ON CLUSTER cluster_3s_2r 
 -- DROP COLUMN IF EXISTS `platform`;
-ALTER TABLE ods.fishpond_conversion_local ON CLUSTER cluster_3s_2r
+ALTER TABLE dim.fishpond_task_local ON CLUSTER cluster_3s_2r
 ADD COLUMN IF NOT EXISTS `platform` String DEFAULT 'jd' AFTER `shop_id`;
 
--- ALTER TABLE ods.fishpond_conversion_all ON CLUSTER cluster_3s_2r
+-- ALTER TABLE dim.fishpond_task_all ON CLUSTER cluster_3s_2r
 -- DROP COLUMN IF EXISTS `platform`;
-ALTER TABLE ods.fishpond_conversion_all ON CLUSTER cluster_3s_2r
-ADD COLUMN IF NOT EXISTS `platform` String AFTER `shop_id`;
-
--- ALTER TABLE buffer.fishpond_conversion_buffer ON CLUSTER cluster_3s_2r
--- DROP COLUMN IF EXISTS `platform`;
-ALTER TABLE buffer.fishpond_conversion_buffer ON CLUSTER cluster_3s_2r
+ALTER TABLE dim.fishpond_task_all ON CLUSTER cluster_3s_2r
 ADD COLUMN IF NOT EXISTS `platform` String AFTER `shop_id`;
--- my-workspace/project/xiaoduo/明察质检/tmp4.sql
+++ my-workspace/project/xiaoduo/明察质检/tmp4.sql
@@ -1,32 +1,31 @@
-SELECT *
+SELECT
+    task_id, shop_id, platform, paid_pv, paid_uv, paid_payment
 FROM (
-        SELECT task_id,
+    SELECT task_id,
+        shop_id,
+        platform,
+        count(1) AS paid_pv,
+        uniqExact(cnick) AS paid_uv,
+        sum(payment) AS paid_payment
+    FROM (
+        SELECT DISTINCT cnick,
+            task_id,
             shop_id,
-            count(1) AS paid_pv,
-            uniqExact(cnick) AS paid_uv,
-            sum(payment) AS paid_payment
-        from (
-                select distinct cnick,
-                    task_id,
-                    shop_id,
-                    payment,
-                    order_id
-                FROM ods.fishpond_conversion_all
-                WHERE `day` >= toYYYYMMDD(addDays(today(), -20))
-                    AND `day` <= toYYYYMMDD(today())
-                    AND platform = 'dy'
-                    AND status = 'paid'
-            ) as etl_order
-        GROUP BY shop_id,
-            task_id
-    )
+            platform,
+            payment,
+            order_id
+        FROM ods.fishpond_conversion_all
+        WHERE `day` >= toYYYYMMDD(addDays(today(), -20))
+            AND `day` <= toYYYYMMDD(today())
+            AND platform = 'dy'
+            AND status = 'paid'
+    ) AS etl_order
+    GROUP BY task_id, shop_id, platform
+)
 WHERE task_id GLOBAL IN (
-        select _id
-        from dim.fishpond_task_all
-        where toYYYYMMDD(today()) between `day` and toYYYYMMDD(
-                addDays(
-                    parseDateTimeBestEffort(left(send_time_end, 10)),
-                    5
-                )
-            )
-    )
+    SELECT _id
+    FROM dim.fishpond_task_all
+    WHERE toYYYYMMDD(today()) BETWEEN `day`
+        AND toYYYYMMDD(addDays(parseDateTimeBestEffort(left(send_time_end, 10)),5))
+        AND platform = 'dy'
+)
