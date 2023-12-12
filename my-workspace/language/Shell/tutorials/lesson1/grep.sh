287dba353d55c126728bfc690ddd6f94274619ae
TomAndersen <1040994898@qq.com>
2023-12-12 17:20:54 +0800

    update `date`

--- my-workspace/language/Shell/tutorials/lesson1/grep.sh
+++ my-workspace/language/Shell/tutorials/lesson1/grep.sh
@@ -6,14 +6,12 @@ set -e
 export PS4='+${LINENO}: '
 set -x
 
-
 # NOTE: grep will return non-zero exit code when no match found
 echo "hello world!" | grep "hello"
 echo $?
 
 echo "hello world!" | grep "la"
 echo $?
-
 
 echo "hello world!" | grep "la" | sort
 echo $?
