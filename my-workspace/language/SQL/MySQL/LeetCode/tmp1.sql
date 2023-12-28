8df67133044cc075b2da712d2c6463aab76ff530
TomAndersen <1040994898@qq.com>
2023-12-28 19:27:22 +0800

    update Thu Dec 28 19:27:22 CST 2023

--- my-workspace/language/SQL/MySQL/LeetCode/tmp1.sql
+++ my-workspace/language/SQL/MySQL/LeetCode/tmp1.sql
@@ -1,4 +1,4 @@
 alter table
     aml_f_product_evaluate_task_calculate_record
 add
-    column current_remain_risk VARCHAR(20) set utf8mb4 collate utf8mb4_bin DEFAULT NULL COMMENT '当前剩余风险';
+    column current_remain_risk VARCHAR(20) character set utf8mb4 collate utf8mb4_bin DEFAULT NULL COMMENT '当前剩余风险';
