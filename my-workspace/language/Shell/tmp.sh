04a6ee66cc3698c567b57b452fd351c67999622d
TomAndersen <1040994898@qq.com>
2023-12-29 19:11:25 +0800

    update Fri Dec 29 19:11:24 CST 2023

--- my-workspace/language/Shell/tmp.sh
+++ my-workspace/language/Shell/tmp.sh
@@ -1,6 +1,16 @@
-hive -e "
+#!/bin/bash
 
--- 执行日期 batch_date
-set mapred.max.split.size=512000000;
-use imd_aml300_ads_safe;
-"
+if [ ! $# -eq 2 ]; then
+    echo "need two date as parameters"
+    exit -9
+fi
+
+batch_date=$(date -d ${1} +%Y-%m-%d 2>/dev/null)
+# 生命周期天数
+ttl_day=${2}
+
+backup_date=$(date -d "$batch_date - $ttl_day days" "+%Y-%m-%d")
+
+echo "backup_date: $backup_date"
+
+echo "{"'"batch_date"':'"'$batch_date'"','"backup_date"':'"'$backup_date'"', '"ttl_day"':'"'$ttl_day'"'"}" 
