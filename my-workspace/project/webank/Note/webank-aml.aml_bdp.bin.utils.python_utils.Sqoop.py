6c9faf7fd2bb397f76c9cccc746502a8116be63b
TomAndersen <1040994898@qq.com>
2024-01-03 20:56:32 +0800

    update Wed Jan  3 20:56:31 CST 2024

--- my-workspace/project/webank/Note/webank-aml.aml_bdp.bin.utils.python_utils.Sqoop.py
+++ my-workspace/project/webank/Note/webank-aml.aml_bdp.bin.utils.python_utils.Sqoop.py
@@ -5,5 +5,46 @@ aml_bdp.bin.utils.python_utils.AmlContext.py
         # 强制增加参数 "--loadMysql"
         newArgs.append("--loadMysql")
         main(newArgs)
-            # 
+
             aml_bdp.bin.utils.python_utils.Sqoop.main
+                # 初始化 amlContext
+                amlContext = AmlContext.instance(args, loadMysql=True)
+                # 
+                if amlContext.hasConf('import_hcatalog'):
+                    importHcatalogPartitionTable(amlContext)
+                        aml_bdp.bin.utils.python_utils.Sqoop.importHcatalogPartitionTable:
+                            #
+                            ...
+                            # 使用 AmlContext.properties 和 env环境变量 转换 shell-style 变量
+                            oriCmd = amlContext.convertEnvVariable(oriCmd)
+                            # 解析 sqoop 命令行参数, AmlContext中解析的是 python 命令行参数
+                            commandLines = CommandLineArgs.parse(re.split('\s+', oriCmd))
+
+                            # 删除分区
+                            if amlContext.hasConf('drop-partition'):
+                                truncateIfExistsHivePartition(commandLines)
+                            
+                            # 删除表
+                            elif amlContext.hasConf('truncate-table'):
+                                truncateTable(commandLines)
+                            
+                            # 格式化 sqoop 命令行
+                            oriCmd = commandLines.convertSpecialWithSpace()
+                            cmds = f"sqoop import " + """ -D mapreduce.job.queuename=${your_queue_name} --connect "${url}?useUnicode=true&characterEncoding=utf-8&tinyInt1isBit=false" --username ${username} --password ${mysql_password} --hive-overwrite --fields-terminated-by '|'  -null-string '\\\\N' -null-non-string '\\\\N' --hcatalog-database ${DATABASE_NAME}  --hcatalog-storage-stanza 'stored as orc tblproperties ("orc.compress"="SNAPPY")' -m 1 """ + f' {oriCmd}'
+
+                            # 替换 shell 命令中的变量
+                            finalCmd = amlContext.convertEnvVariable(cmds)
+                            # 执行 shell 命令
+                            commonUtil.execShell(finalCmd)
+
+                elif amlContext.hasConf('import_txt'):
+                    importTxt(amlContext)
+                # 
+                elif amlContext.hasConf('import_txt_by_log'):
+                    importTxtByHcatalog(amlContext)
+                # 
+                elif amlContext.hasConf('export_hcatalog'):
+                    exportHcatalog(amlContext)
+                # 
+                elif amlContext.hasConf('export_txt'):
+                    exportTxt(amlContext)
--- my-workspace/project/webank/Shell/tmp.sh
+++ my-workspace/project/webank/Shell/tmp.sh
@@ -1,12 +1,12 @@
 
 
 webank-aml 数据同步任务客户端:
-    aml_bdp/bin/utils/python_utils/etlTool.py --type=spark
+    aml_bdp/bin/utils/python_utils/etlTool.py | HAML_PYTHON_ETL_TOOL --type=spark
     aml_bdp/bin/utils/bash_utils/etl_submit.sh --type=sqoop
     aml_bdp/bin/utils/bash_utils/etl_submit.sh --type=spark
     aml_bdp/bin/utils/bash_utils/etl_submit.sh --type=blanca
         SparkJob
         Jdbc
-
-    HAML_PYTHON_SQOOP_TOOL
+    
+    aml_bdp/bin/utils/python_utils/Sqoop.py | HAML_PYTHON_SQOOP_TOOL
     sqoop/import/blacklist/operator/import_mlist_type.py
